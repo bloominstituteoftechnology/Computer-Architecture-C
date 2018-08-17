daf529c890cd82e36f722e18dac4813e99e4d014
Beej Jorgensen <beej@lambdaschool.com>
2018-08-17 16:02:43 -0700

    remove filename from prototype

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -19,7 +19,7 @@ struct cpu {
 
 // Function declarations
 
-extern void cpu_load(char *filename, struct cpu *cpu);
+extern void cpu_load(struct cpu *cpu);
 extern void cpu_init(struct cpu *cpu);
 extern void cpu_run(struct cpu *cpu);
 
