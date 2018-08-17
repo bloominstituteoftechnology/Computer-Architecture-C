dbd848edb08e1a79a153df6067d166f6cd4a8938
Beej Jorgensen <beej@lambdaschool.com>
2018-08-16 20:04:00 -0700

    Initial add

---
+++ ls8/cpu.h
@@ -0,0 +1,28 @@
+#ifndef _CPU_H_
+#define _CPU_H_
+
+#include <sys/time.h>
+
+// Holds all information about the CPU
+struct cpu {
+  // TODO
+  // PC
+  // registers (array)
+  // ram (array)
+};
+
+// Instructions
+
+// These use binary literals. If these aren't available with your compiler, hex
+// literals should be used.
+
+#define LDI  0b10000010
+// TODO: more instructions here. These can be used in cpu_run().
+
+// Function declarations
+
+extern void cpu_load(char *filename, struct cpu *cpu);
+extern void cpu_init(struct cpu *cpu);
+extern void cpu_run(struct cpu *cpu);
+
+#endif
