271741b3fc770d6cc4769147a263128d9ad5413f
jesuarva <jesuarva.developer@gmail.com>
2018-08-20 23:13:32 +0200

    DEFINE a helper function `void handle_instruction(struct cpu *cpu, char *IR)`
    This function is aim to hanlde the `switch` inside the 'cpu_run()'

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,5 +1,6 @@
 #include "cpu.h"
 
+void handle_instruction(struct cpu *cpu, char *IR);
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
@@ -49,6 +50,7 @@ void cpu_run(struct cpu *cpu)
   int running = 1; // True until we get a HLT instruction
   char *rgstr = cpu->reg;
   char *ram = cpu->ram;
+
   char *PC = cpu->guts->PC;
   char *IR = cpu->guts->IR;
   char *MAR = cpu->guts->MAR;
@@ -60,6 +62,7 @@ void cpu_run(struct cpu *cpu)
     // TODO
     // 1. Get the value of the current instruction (in address PC).
     // 2. switch() over it to decide on a course of action.
+    handle_instruction(cpu, IR);
     // 3. Do whatever the instruction should do according to the spec.
     // 4. Move the PC to the next instruction.
   }
@@ -74,3 +77,52 @@ void cpu_init(struct cpu *cpu)
 
   // TODO: Zero registers and RAM
 }
+
+/**
+ * HELPERS
+*/
+void handle_instruction(struct cpu *cpu, char *IR)
+{
+  switch (*IR)
+  {
+    // ALU
+  case ADD:
+  case SUB:
+  case MUL:
+  case DIV:
+  case MOD:
+  case INC:
+  case DEC:
+  case CMP:
+  case AND:
+  case NOT:
+  case OR:
+  case XOR:
+  case SHL:
+  case SHR:
+  // PC mutators
+  case CALL:
+  case RET:
+  case INT:
+  case IRET:
+  case JMP:
+  case JEQ:
+  case JNE:
+  case JGT:
+  case JLT:
+  case JLE:
+  case JGE:
+  // Other
+  case NOP:
+  case HLT:
+  case LDI:
+  case LD:
+  case ST:
+  case PUSH:
+  case POP:
+  case PRN:
+  case PRA:
+  default:
+    printf("The %s IR instruction has no handler", IR);
+  }
+};
