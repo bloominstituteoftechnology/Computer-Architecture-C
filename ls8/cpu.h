9399cc2a0b79db00964e2e2a64605657441f07b0
Bee C <b.choeung@gmail.com>
2018-08-20 19:08:45 -0400

    added ALU ops to cpu.h

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -5,7 +5,7 @@
 struct cpu {
   // TODO
   // PC
-
+  int pc[8];
   // registers (array)
   unsigned char reg[8];
   // ram (array)
@@ -19,9 +19,16 @@ struct cpu {
 
 #define LDI  0b10000010 //represents 8-bit value in binary literal
 // TODO: more instructions here. These can be used in cpu_run().
+#define ADD  10100000
+#define SUB  10100001
+#define MUL  10100010
+#define DIV  10100011
+#define MOD  10100100
+#define INC  01100101
+#define DEC  01100110
+#define CMP  10100111
 
 // Function declarations
-
 extern void cpu_load(struct cpu *cpu);
 extern void cpu_init(struct cpu *cpu);
 extern void cpu_run(struct cpu *cpu);
