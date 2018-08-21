74be313aeba855b5833b98816d35948a3c89f40a
Mason Morrow <A99@99s-MacBook-Pro.local>
2018-08-22 00:16:39 +0200

    Bug fixes: update to solution lecture

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,4 +1,7 @@
 #include "cpu.h"
+#include <stdlib.h>
+#include <stdio.h>
+
  
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
@@ -42,18 +45,18 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 /**
  * CPU Read
  */
-void cpu_read(struct cpu *cpu)
+char cpu_read(struct cpu *cpu, unsigned char address)
 {
-  //cpu-ram
+  return cpu->ram[address];
 }
 
 
 /**
  * CPU Write
  */
-void cpu_write(struct cpu *cpu)
+void cpu_write(struct cpu *cpu, unsigned char address, unsigned char value)
 {
-  //cpu->ram
+  cpu->ram[address] = value;
 }
 
 
@@ -66,10 +69,34 @@ void cpu_run(struct cpu *cpu)
 
   while (running) {
     // TODO
-    // 1. Get the value of the current instruction (in address PC). // array[0]
-    // 2. switch() over it to decide on a course of action. // call alu?
+    // 1. Get the value of the current instruction (in address PC). //
+    unsigned char IR = cpu_read(cpu, cpu->pc);
+    unsigned char operandA = cpu_read(cpu, cpu->pc+1);
+    unsigned char operandB = cpu_read(cpu, cpu->pc+2);
+    // 2. switch() over it to decide on a course of action.
+
+    switch (IR)
+    {
+      case LDI:
+        cpu->reg[operandA] = operandB;
+        break;
+
+      case PRN:
+        printf("%d\n", cpu->reg[operandA]);
+        break;
+
+      case HLT:
+        running = 0;
+        break;
+    
+      default:
+        printf("unknown instruction at %02x: %02x\n", cpu->pc, IR);
+        exit(2);
+    }
     // 3. Do whatever the instruction should do according to the spec.
     // 4. Move the PC to the next instruction.
+    cpu->pc += (IR >> 6) + 1;
+
   }
 }
 
@@ -79,6 +106,6 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
-
+  cpu->pc = 0;
   // TODO: Zero registers and RAM
 }
