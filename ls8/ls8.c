734402307062ca932b1faf889ab826e61f403ef8
bbenefield89 <bsquared18@gmail.com>
2018-08-21 19:00:37 -0400

    PASS FILE FROM `argv[1]` POINTER
    - in ls8.c

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -5,13 +5,13 @@
 /**
  * Main
  */
-int main(void)
+int main(int argc, char **argv)
 {
-  struct cpu cpu;
+    struct cpu cpu;
 
-  cpu_init(&cpu);
-  cpu_load(&cpu);
-  cpu_run(&cpu);
+    cpu_init(&cpu);
+    cpu_load(&cpu, argv[1]);
+    cpu_run(&cpu);
 
-  return 0;
+    return 0;
 }
