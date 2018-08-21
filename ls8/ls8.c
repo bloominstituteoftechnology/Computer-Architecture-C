c93c641d18998eefe50afb963fe2526c05eac770
JSNavey <jsnavey@gmail.com>
2018-08-21 16:57:57 -0400

    implement argc and argv in ls8.c

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "cpu.h"
 
 /**
  * Main
  */
-int main(void)
+int main(int argc, char *argv[])
 {
   struct cpu cpu;
+
+  char *filename;
+
+  if (argc == 2)
+  {
+    filename = argv[1];
+  }
+  else
+  {
+    fprintf(stderr, "./ls8 examples/file.ls8\n");
+    exit(1);
+  }
 
   cpu_init(&cpu);
   cpu_load(&cpu);
   cpu_run(&cpu);
+
 
   return 0;
 }
