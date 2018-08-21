f839715817dffac2bc799476f6770bc5f759d0fa
Danny Vail <danny_vail@outlook.com>
2018-08-21 02:30:47 -0400

    ADDS HLT INSTRUCTION HANDLER In cpu.c - Implements handle_HLT() which halts the CPU and exits the emulator - Moved alu() to top of file

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,6 +1,30 @@
 #include "cpu.h"
 
 /**
+ * ALU
+ */
+// void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
+// {
+//   switch (op) {
+//     case ALU_MUL:
+//       // TODO
+//       break;
+
+//     // TODO: implement more ALU ops
+//   }
+// }
+
+/**
+ * CPU Instruction: Halts the CPU and exits emulator
+ * 
+ * @param running {int*} Pointer to cpu loop variable.
+ */
+void handle_HLT(int *running)
+{
+  *running = 0;
+}
+
+/**
  * Reads from RAM address and stores value into store
  * 
  * @param ram {unsigned char[]} Memory array.
@@ -101,17 +125,3 @@ void cpu_run(struct cpu *cpu)
     // 4. Move the PC to the next instruction.
   }
 }
-
-/**
- * ALU
- */
-// void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
-// {
-//   switch (op) {
-//     case ALU_MUL:
-//       // TODO
-//       break;
-
-//     // TODO: implement more ALU ops
-//   }
-// }
