3fb7a07c27c9c66979fca15408c76cfe51a85a94
Danny Vail <danny_vail@outlook.com>
2018-08-21 02:42:02 -0400

    ADDS PRN INSTRUCTION HANDLER In cpu.c - Implements handle_PRN() which prints the numeric value stored in the given register

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -25,7 +25,7 @@ void handle_HLT(int *running)
 }
 
 /**
- * CPU Instruction: Halts the CPU and exits emulator
+ * CPU Instruction: Sets value of register to an integer
  * 
  * @param registers {unsigned char[]} Register array.
  * @param reg {unsigned char} Register number.
@@ -34,6 +34,17 @@ void handle_HLT(int *running)
 void handle_LDI(unsigned char registers[], unsigned char reg, unsigned char immediate)
 {
   registers[reg] = immediate;
+}
+
+/**
+ * CPU Instruction: Prints numeric value stored in given register
+ * 
+ * @param registers {unsigned char[]} Register array.
+ * @param reg {unsigned char} Register number.
+ */
+void handle_PRN(unsigned char registers[], unsigned char reg)
+{
+  printf("%d\n", registers[reg]);
 }
 
 /**
