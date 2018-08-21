91094c951c99eb1acfa767a643f2f2a002358a17
bbenefield89 <bsquared18@gmail.com>
2018-08-21 16:57:28 -0400

    WRITE VALUE TO REGISTER
    - in cpu.c

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -59,154 +59,53 @@ void cpu_run(struct cpu* cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
+  unsigned char program_counter;
+  unsigned char instruction_register;
+  unsigned char operandA;
+  unsigned char operandB;
+  unsigned int next_instruction;
+
   while (running) {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
-    int pc = cpu->pc;
+    program_counter      = cpu->pc;
+    instruction_register = cpu_ram_read(cpu, cpu->pc);
+
     // 2. switch() over it to decide on a course of action.
-    switch(pc)
+    switch(instruction_register >> 6)
     {
-        case ADD:
-            printf("ADD");
+        case 1:
+            printf("1");
+
+            operandA = instruction_register + 1;
             break;
 
-        case AND:
-            printf("AND");
-            break;
+        case 2:
+            printf("\n2\n\n");
 
-        case CALL:
-            printf("CALL");
-            break;
+            next_instruction = 3;
+            operandA = cpu_ram_read(cpu, cpu->pc + 1);
+            operandB = cpu_ram_read(cpu, cpu->pc + 2);
 
-        case CMP:
-            printf("CMP");
-            break;
+            switch(instruction_register)
+            {
+                case LDI:
+                    cpu->registers[ operandA ] = operandB;
+                    break;
 
-        case DEC:
-            printf("DEC");
-            break;
-
-        case DIV:
-            printf("DIV");
-            break;
-
-        case HLT:
-            printf("HLT");
-            break;
-
-        case INC:
-            printf("INC");
-            break;
-
-        case INT:
-            printf("INT");
-            break;
-
-        case IRET:
-            printf("IRET");
-            break;
-
-        case JEQ:
-            printf("JEQ");
-            break;
-
-        case JGE:
-            printf("JGE");
-            break;
-
-        case JGT:
-            printf("JGT");
-            break;
-
-        case JLE:
-            printf("JLE");
-            break;
-
-        case JLT:
-            printf("JLT");
-            break;
-
-        case JMP:
-            printf("JMP");
-            break;
-
-        case JNE:
-            printf("JNE");
-            break;
-
-        case LD:
-            printf("LD");
-            break;
-
-        case LDI:
-            printf("LDI");
-            break;
-
-        case MOD:
-            printf("MOD");
-            break;
-
-        case MUL:
-            printf("MUL");
-            break;
-
-        case NOP:
-            printf("NOP");
-            break;
-
-        case NOT:
-            printf("NOT");
-            break;
-
-        case OR:
-            printf("OR");
-            break;
-
-        case POP:
-            printf("POP");
-            break;
-
-        case PRA:
-            printf("PRA");
-            break;
-
-        case PRN:
-            printf("PRN");
-            break;
-
-        case PUSH:
-            printf("PUSH");
-            break;
-
-        case RET:
-            printf("RET");
-            break;
-
-        case SHL:
-            printf("SHL");
-            break;
-
-        case SHR:
-            printf("SHR");
-            break;
-
-        case ST:
-            printf("ST");
-            break;
-
-        case SUB:
-            printf("SUB");
-            break;
-
-        case XOR:
-            printf("XOR");
+                default:
+                    break;
+            }
+            
             break;
 
         default:
-            printf("\n%d\n", pc);
+            printf("HLT");
+            running = 0;
     }
-
-    running--;
+    
+    cpu->pc = cpu->pc + next_instruction;
+    
     // 3. Do whatever the instruction should do according to the spec.
     // 4. Move the PC to the next instruction.
   }
@@ -218,6 +117,149 @@ void cpu_run(struct cpu* cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
+  cpu->pc = 0;
 
   // TODO: Zero registers and RAM
 }
+
+    // switch(instruction_register)
+    // {
+    //     case ADD:
+    //         printf("ADD");
+    //         break;
+
+    //     case AND:
+    //         printf("AND");
+    //         break;
+
+    //     case CALL:
+    //         printf("CALL");
+    //         break;
+
+    //     case CMP:
+    //         printf("CMP");
+    //         break;
+
+    //     case DEC:
+    //         printf("DEC");
+    //         break;
+
+    //     case DIV:
+    //         printf("DIV");
+    //         break;
+
+    //     case HLT:
+    //         printf("HLT");
+    //         break;
+
+    //     case INC:
+    //         printf("INC");
+    //         break;
+
+    //     case INT:
+    //         printf("INT");
+    //         break;
+
+    //     case IRET:
+    //         printf("IRET");
+    //         break;
+
+    //     case JEQ:
+    //         printf("JEQ");
+    //         break;
+
+    //     case JGE:
+    //         printf("JGE");
+    //         break;
+
+    //     case JGT:
+    //         printf("JGT");
+    //         break;
+
+    //     case JLE:
+    //         printf("JLE");
+    //         break;
+
+    //     case JLT:
+    //         printf("JLT");
+    //         break;
+
+    //     case JMP:
+    //         printf("JMP");
+    //         break;
+
+    //     case JNE:
+    //         printf("JNE");
+    //         break;
+
+    //     case LD:
+    //         printf("LD");
+    //         break;
+
+    //     case LDI:
+    //         printf("LDI");
+    //         break;
+
+    //     case MOD:
+    //         printf("MOD");
+    //         break;
+
+    //     case MUL:
+    //         printf("MUL");
+    //         break;
+
+    //     case NOP:
+    //         printf("NOP");
+    //         break;
+
+    //     case NOT:
+    //         printf("NOT");
+    //         break;
+
+    //     case OR:
+    //         printf("OR");
+    //         break;
+
+    //     case POP:
+    //         printf("POP");
+    //         break;
+
+    //     case PRA:
+    //         printf("PRA");
+    //         break;
+
+    //     case PRN:
+    //         printf("PRN");
+    //         break;
+
+    //     case PUSH:
+    //         printf("PUSH");
+    //         break;
+
+    //     case RET:
+    //         printf("RET");
+    //         break;
+
+    //     case SHL:
+    //         printf("SHL");
+    //         break;
+
+    //     case SHR:
+    //         printf("SHR");
+    //         break;
+
+    //     case ST:
+    //         printf("ST");
+    //         break;
+
+    //     case SUB:
+    //         printf("SUB");
+    //         break;
+
+    //     case XOR:
+    //         printf("XOR");
+    //         break;
+
+    //     default:
+    //         printf("\n%d\n", instruction_register);
+    // }
