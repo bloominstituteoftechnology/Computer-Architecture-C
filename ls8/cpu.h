db9819bbb607b90beb23064939e368a243d30c82
ckopecky <ckopecky@github.com>
2018-08-20 18:13:51 -0700

    Take out Hex Literal . Logical ops apparently don't work in #defines

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -50,43 +50,42 @@ struct CPU {
 // literals should be used.
 
 //'0b' in front of a binary number indicates binary literal...can also be '0B'. 
-// '0x' in front of a hex number indicates hexadecimal literal...can also be '0X'
-
-#define LDI  0b10000010 || 0x82; 
-#define ADD 0b10100000 || 0xA0;
-#define AND 0b10101000 || 0xA8;
-#define CALL 0b01010000 || 0x50;
-#define CMP 0b10100111 || 0xA7;
-#define DEC 0b01100110 || 0x66;
-#define DIV 0b10100011 || 0xA3;
-#define HLT 0b00000001 || 0x01;
-#define INC 0b01100101 || 0x65;
-#define INT 0b01010010 || 0x52;
-#define IRET 0b00010011 || 0x13;
-#define JEQ 0b01010101 || 0x55;
-#define JGE 0b01011010 || 0x5A;
-#define JGT 0b01010111 || 0x57;
-#define JLE 0b01011001 || 0x59;
-#define JLT 0b01011000 || 0x58;
-#define JMP 0b01010100 || 0x54;
-#define JNE 0b01010110 || 0x56;
-#define LD 0b10000011 || 0x83;
-#define MOD 0b10100100 || 0xA4;
-#define MUL 0b10100010 || 0xA2;
-#define NOP 0b00000000 || 0x00;
-#define NOT 0b01101001 || 0x69;
-#define OR 0b10101010 || 0xAA;
-#define POP 0b01000110 || 0x46;
-#define PRA 0b01001000 || 0x48;
-#define PRN 0b01000111 || 0x47;
-#define PUSH 0b01000101 || 0x45;
-#define RET 0b00010001 || 0x11;
-#define SHL 0b10101100 || 0xAC;
-#define SHR 0b10101101 || 0xAD;
-#define ST 0b10000100 || 0x84;
-#define SUB 0b10100001 || 0xA1;
-#define XOR 0b10101011 || 0xAB;
-// TODO: more instructions here. These can be used in cpu_run().
+// '0x' in front of a hr indicates hexadecimal literal...can also be '0X'
+#define LDI  0b10000010 
+#define ADD 0b10100000
+#define AND 0b10101000
+#define CALL 0b01010000
+#define CMP 0b10100111
+#define DEC 0b01100110
+#define DIV 0b10100011
+#define HLT 0b00000001
+#define INC 0b01100101
+#define INT 0b01010010
+#define IRET 0b00010011
+#define JEQ 0b01010101
+#define JGE 0b01011010
+#define JGT 0b01010111
+#define JLE 0b01011001
+#define JLT 0b01011000
+#define JMP 0b01010100
+#define JNE 0b01010110
+#define LD 0b10000011
+#define MOD 0b10100100
+#define MUL 0b10100010
+#define NOP 0b00000000
+#define NOT 0b01101001
+#define OR 0b10101010
+#define POP 0b01000110
+#define PRA 0b01001000
+#define PRN 0b01000111
+#define PUSH 0b01000101
+#define RET 0b00010001
+#define SHL 0b10101100
+#define SHR 0b10101101
+#define ST 0b10000100
+#define SUB 0b10100001
+#define XOR 0b10101011
+// TODO: more instructi These can be used in cpu_run().
 
 // Function declarations
 
