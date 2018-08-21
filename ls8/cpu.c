97d2c5cbf094f746913308c07983ca753bbb8841
bbenefield89 <bsquared18@gmail.com>
2018-08-21 15:29:19 -0400

    IMPLEMENT RAM FUNCTIONS
    - in cpu.c

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,12 +1,25 @@
 #include "cpu.h"
 
 /**
+ * read data from RAM
+ */
+unsigned char cpu_ram_read(struct cpu* cpu, unsigned char address)
+{
+    return cpu->ram[ address ];
+}
+
+void cpu_ram_write(struct cpu* cpu, unsigned char address, unsigned char value)
+{
+    cpu->ram[ address ] = value;
+}
+
+/**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
 void cpu_load(struct cpu *cpu)
 {
   const int DATA_LEN = 6;
-  char data[DATA_LEN] = {
+  char data[sizeof(char) * 6] = {
     // From print8.ls8
     0b10000010, // LDI R0,8
     0b00000000,
@@ -28,47 +41,175 @@ void cpu_load(struct cpu *cpu)
 /**
  * ALU
  */
-void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
-{
-  switch (op) {
-    case ALU_MUL:
-      // TODO
-      break;
+// void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
+// {
+//   switch (op) {
+//     case ALU_MUL:
+//       // TODO
+//       break;
 
-    // TODO: implement more ALU ops
-  }
-}
+//     // TODO: implement more ALU ops
+//   }
+// }
 
 /**
  * Run the CPU
  */
-void cpu_run(struct cpu *cpu)
+void cpu_run(struct cpu* cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
   while (running) {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
+    int pc = cpu->pc;
     // 2. switch() over it to decide on a course of action.
+    switch(pc)
+    {
+        case ADD:
+            printf("ADD");
+            break;
+
+        case AND:
+            printf("AND");
+            break;
+
+        case CALL:
+            printf("CALL");
+            break;
+
+        case CMP:
+            printf("CMP");
+            break;
+
+        case DEC:
+            printf("DEC");
+            break;
+
+        case DIV:
+            printf("DIV");
+            break;
+
+        case HLT:
+            printf("HLT");
+            break;
+
+        case INC:
+            printf("INC");
+            break;
+
+        case INT:
+            printf("INT");
+            break;
+
+        case IRET:
+            printf("IRET");
+            break;
+
+        case JEQ:
+            printf("JEQ");
+            break;
+
+        case JGE:
+            printf("JGE");
+            break;
+
+        case JGT:
+            printf("JGT");
+            break;
+
+        case JLE:
+            printf("JLE");
+            break;
+
+        case JLT:
+            printf("JLT");
+            break;
+
+        case JMP:
+            printf("JMP");
+            break;
+
+        case JNE:
+            printf("JNE");
+            break;
+
+        case LD:
+            printf("LD");
+            break;
+
+        case LDI:
+            printf("LDI");
+            break;
+
+        case MOD:
+            printf("MOD");
+            break;
+
+        case MUL:
+            printf("MUL");
+            break;
+
+        case NOP:
+            printf("NOP");
+            break;
+
+        case NOT:
+            printf("NOT");
+            break;
+
+        case OR:
+            printf("OR");
+            break;
+
+        case POP:
+            printf("POP");
+            break;
+
+        case PRA:
+            printf("PRA");
+            break;
+
+        case PRN:
+            printf("PRN");
+            break;
+
+        case PUSH:
+            printf("PUSH");
+            break;
+
+        case RET:
+            printf("RET");
+            break;
+
+        case SHL:
+            printf("SHL");
+            break;
+
+        case SHR:
+            printf("SHR");
+            break;
+
+        case ST:
+            printf("ST");
+            break;
+
+        case SUB:
+            printf("SUB");
+            break;
+
+        case XOR:
+            printf("XOR");
+            break;
+
+        default:
+            printf("\n%d\n", pc);
+    }
+
+    running--;
     // 3. Do whatever the instruction should do according to the spec.
     // 4. Move the PC to the next instruction.
   }
-}
-
-/**
- * read data from RAM
- */
-void cpu_ram_read(struct cpu *cpu)
-{
-
-}
-
-/**
- * write data to RAM
- */
-void cpu_ram_write(struct cpu *cpu)
-{
-
 }
 
 /**
