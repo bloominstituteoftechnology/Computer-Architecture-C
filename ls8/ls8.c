c1588142a8dba7e3cba85c9b210f5c67bcd461df
ckopecky <ckopecky@github.com>
2018-08-21 14:30:58 -0700

    Error handling if argc is less than 2

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -9,10 +9,16 @@
 int main(int argc, char **argv)
 {
   struct CPU cpu;
-
+  if (argc < 2) {
+    fprintf(stderr, "usage: ./ls8 + example/<programfile> \n");
+  }
+  else {
   cpu_init(&cpu);
   cpu_load(&cpu);
   cpu_run(&cpu);
+  }
+
+
 
   return 0;
 }
