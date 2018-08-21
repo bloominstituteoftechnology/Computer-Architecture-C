3106834ea4d741b8112bf7c23ad2574f7456adad
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-08-21 15:07:14 -0700

    include command line args in main function; work on un-hardcoding cpu_load

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -16,26 +16,22 @@ void cpu_ram_write(struct cpu *cpu, unsigned char address, unsigned char value)
   cpu->ram[address] = value;
 }
 
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char *file)
 {
-  const int DATA_LEN = 6;
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+  FILE * f;
+  f = fopen(file, "r");
 
   int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  char line[256];
+
+  while (fgets(line, sizeof(line), f)) {
+    int i = 0;
+    printf("%s", line);
+    cpu->ram[address++] = line;
   }
 
-  // TODO: Replace this with something less hard-coded
+  fclose(f);
 }
 
 /**
@@ -94,6 +90,6 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
-
+  cpu->pc = 0;
   // TODO: Zero registers and RAM
 }
