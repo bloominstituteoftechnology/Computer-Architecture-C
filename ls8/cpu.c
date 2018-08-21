0226436de3c90ea18e0ca48aa83131e6afe19351
AmyShackles <amyshackles@gmail.com>
2018-08-21 16:10:19 -0700

    Now have working stack.

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -31,22 +31,32 @@ void cpu_load(char *filename, struct cpu *cpu)
   FILE *fp; // file pointer
   char line[256]; // storage for line of file
   int counter = 0; // way to track index in ram
-  char *ptr; // pointer to next character in line after the unsigned long integer
 
   if ((fp = fopen(filename, "r")) == NULL) {
     fprintf(stderr, "Cannot open file %s\n", filename);
     exit(1);
   }
     while (fgets(line, sizeof(line), fp) != NULL) { // read line from file and store in line up to 256 bytes.
-      cpu->ram[counter++] = strtoul(line, &ptr, 2); // converts string to unsigned long integer using base 2 and stores in ram
+      char *ptr;
+      unsigned char byte = strtoul(line, &ptr, 2);
+      if (ptr == line) {
+        continue;
+      }
+      cpu->ram[counter++] = byte; // converts string to unsigned long integer using base 2 and stores in ram
     }
+    // for (unsigned long i = 0; i < 256; i++) {
+    //   printf("What is this? %u\n", cpu->ram[i]);
+    // }
   // TODO: Replace this with something less hard-coded
 }
 
 unsigned char cpu_ram_read(struct cpu *cpu, int index){
   return cpu->ram[index];
 }
-void cpu_ram_write(struct cpu *cpu);
+void cpu_ram_write(struct cpu *cpu, unsigned char address, unsigned char value)
+{
+  cpu->ram[address] = value;
+}
 
 
 /**
@@ -85,10 +95,11 @@ void cpu_run(struct cpu *cpu)
   unsigned char *reg = cpu->reg;
   unsigned char PC = cpu->PC;
   int running = 1; // True until we get a HLT instruction
+  unsigned char SP = cpu->reg[7];
   
   while (running) {
     unsigned char IR = cpu_ram_read(cpu, PC);
-    int difference = (IR >> 6) + 1; // shifts the number 6 places to the right (leaving last two places)
+    int difference = ((IR >> 6) & 0b11) + 1; // shifts the number 6 places to the right (leaving last two places)
     // since the number of operands can be found in the two high bits, add one for opcode to get to next instruction
     unsigned char operandA = cpu_ram_read(cpu, PC+1);
     unsigned char operandB = cpu_ram_read(cpu, PC+2);
@@ -97,22 +108,30 @@ void cpu_run(struct cpu *cpu)
     {
       case LDI:
         reg[operandA] = operandB;
-        PC+=difference;
+        // PC+=difference;
         break;
       case PRN:
         printf("%d\n", reg[operandA]);
-        PC+=difference;
+        // PC+=difference;
         break;
       case MUL:
         alu(cpu, ALU_MUL, operandA, operandB);
-        PC+= difference;
+        // PC+= difference;
+        break;
+      case POP:
+        reg[operandA] = cpu->ram[SP++];
+        break;
+      case PUSH:
+        cpu_ram_write(cpu, --SP, reg[operandA]);
         break;
       case HLT:
         running = 0;
         break;
       default:
-          break;
+        printf("Unknown instruction at %02x: %02x\n", cpu->PC, IR);
+        exit(2);
     }
+    PC+=difference;
     // TODO
     // 1. Get the value of the current instruction (in address PC).
     // 2. switch() over it to decide on a course of action.
@@ -127,7 +146,8 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   cpu->PC = 0;
-
+  cpu->reg = (unsigned char *) calloc(8, sizeof(unsigned char));
+  cpu->ram = (unsigned char *) calloc(256, sizeof(unsigned char));
   // TODO: Initialize the PC and other special registers
 
   // TODO: Zero registers and RAM
