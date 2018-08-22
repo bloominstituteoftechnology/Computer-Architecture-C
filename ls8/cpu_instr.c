edb82f4cb025a963b4ab3843bd17757fdce9959f
Danny Vail <danny_vail@outlook.com>
2018-08-22 01:16:28 -0400

    UPDATE CPU INSTRUCTION FILE In cpu_instr.c - Includes standard library and cpu header - Renames parameter for handle_HLT - Updates parameter list for handle_LDI and handle_PRN to match handler typedef - Adds load_cpu_instructions which builds out the branch_table

--- ls8/cpu_instr.c
+++ ls8/cpu_instr.c
@@ -1,35 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "cpu.h"
 #include "cpu_instr.h"
 
 /**
  * CPU Instruction: Halts the CPU and exits emulator
  * 
- * @param running {int*} Pointer to cpu loop variable.
+ * @param status {int*} Pointer to cpu loop variable.
  */
-void handle_HLT(int *running)
+void handle_HLT(int *status)
 {
-  *running = 0;
+  *status = 0;
 }
 
 /**
  * CPU Instruction: Sets value of register to an integer
  * 
- * @param registers {unsigned char[]} Register array.
- * @param reg {unsigned char} Register number.
- * @param immediate {unsigned char} value to be stored in register.
+ * @param cpu {struct cpu*} Pointer to a cpu struct.
+ * @param opA {unsigned char} Operand A: register.
+ * @param opB {unsigned char} Operand B: immediate.
  */
-void handle_LDI(unsigned char registers[], unsigned char reg, unsigned char immediate)
+void handle_LDI(struct cpu *cpu, unsigned char opA, unsigned char opB)
 {
-  registers[reg] = immediate;
+  cpu->registers[opA] = opB;
 }
 
 /**
  * CPU Instruction: Prints numeric value stored in given register
  * 
- * @param registers {unsigned char[]} Register array.
- * @param reg {unsigned char} Register number.
+ * @param cpu {struct cpu*} Pointer to a cpu struct.
+ * @param opA {unsigned char} Operand A: register.
+ * @param opB {unsigned char} Operand B: N/A
  */
-void handle_PRN(unsigned char registers[], unsigned char reg)
+void handle_PRN(struct cpu *cpu, unsigned char opA, unsigned char opB)
 {
-  printf("%d\n", registers[reg]);
+  printf("%d\n", cpu->registers[opA]);
+}
+
+/**
+ * Loads CPU instructions into branch table
+ * 
+ * @param bt {handler*} Pointer to an array of function pointers.
+ */
+void load_cpu_instructions(handler *bt)
+{
+  bt[LDI] = handle_LDI;
+  bt[PRN] = handle_PRN;
 }
