bdd557a63ff374b5d196a694f92a474139d89c51
A.D.Faris <adfaris@gmail.com>
2018-08-22 18:27:57 -0400

    Stack Complete

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,6 +1,8 @@
 #include "cpu.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 unsigned char cpu_ram_read(struct cpu *cpu, unsigned char MAR)
 {
@@ -14,40 +16,85 @@ void cpu_ram_write(struct cpu *cpu, unsigned char MAR, unsigned char MDR)
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+// void cpu_load(struct cpu *cpu, char *argv[])
+// {
+//   // file pointer
+//   FILE *fp;
+//   int address = 0;
+//   char read[100];
+//   printf("%sFILENAME\n", argv[1]);
+//   fp = fopen(argv[1], "r");
+//   if (fp == NULL)
+//   {
+//     fprintf(stderr, "Filepointer goofed\n");
+//     exit(1);
+//   }
+
+//   // loop until fgets is at the end of the file
+//   while (fgets(read, sizeof(read), fp))
+//   {
+//     unsigned long instruction;
+//     char *endptr;
+//     // cast line to binary until instruction is captured
+//     instruction = strtoul(read, &endptr, 2);
+//     // write instruction to RAM @address
+//     if (!(*read == *endptr))
+//       cpu_ram_write(cpu, address++, instruction);
+//   }
+// }
+void cpu_load(struct cpu *cpu, char *argv[])
 {
-  const int DATA_LEN = 6;
-  char data[6] = {
-      // From print8.ls8
-      // 0b10000010, // LDI R0,8
-      // 0b00000000,
-      // 0b00001000,
-      // 0b01000111, // PRN R0
-      // 0b00000000,
-      // 0b00000001 // HLT
 
-      10000010, //# LDI R0,8
-      00000000,
-      00001000,
-      10000010, //# LDI R1,9
-      00000001,
-      00001001,
-      10100010, // # MUL R0,R1
-      00000000,
-      00000001,
-      01000111, // # PRN R0
-      00000000,
-      00000001, // # HLT
-  };
-
+  // cpu->reg[7] = 0xF4; //cpu_load
+  char line[100];
   int address = 0;
-
-  for (int i = 0; i < DATA_LEN; i++)
+  FILE *fp = fopen(argv[1], "r");
+  // printf("%s\n", filename);
+  if (fp == NULL)
   {
-    cpu->ram[address++] = data[i];
+    fprintf(stderr, "File pointer is  not working");
+    exit(1);
   }
+  while (fgets(line, sizeof(line), fp))
+  {
+    char *endchar;
+    unsigned long v;
+    v = strtoul(line, &endchar, 2);
+    if (!(*line == *endchar))
+      cpu_ram_write(cpu, address++, v);
 
-  // TODO: Replace this with something less hard-coded
+    // {
+
+    //   continue;
+  }
+  //     // printf("%u", v);
+  //   }
+
+  //   // const int DATA_LEN = 6;
+  //   // char data[6] = {
+  //   //     // From print8.ls8
+  //   //     0b10000010, // LDI R0,8
+  //   //     0b00000000,
+  //   //     0b00001000,
+  //   //     0b01000111, // PRN R0
+  //   //     0b00000000,
+  //   //     0b00000001 // HLT
+  //   // };
+
+  //   // int address = 0;
+
+  //   // for (int i = 0; i < DATA_LEN; i++)
+  //   // {
+  //   //   cpu->ram[address++] = data[i];
+  //   // }
+
+  //   // TODO:
+  //   // Replace this with something less hard - coded
+  //   for (int i = 0; i < 5; i++)
+  //   {
+  //     printf("I am here in CPU_LOAD\n");
+  //     printf("%02X: %02X\n", i, cpu->ram[i]);
+  //   }
 }
 
 /**
@@ -58,7 +105,7 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
   switch (op)
   {
   case ALU_MUL:
-    cpu->reg[regA] = cpu->reg[regA] * cpu->reg[regB];
+    cpu->reg[regA] *= cpu->reg[regB];
     break;
 
   case ALU_ADD:
@@ -72,15 +119,6 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
   case ALU_SUB:
     cpu->reg[regA] = cpu->reg[regA] - cpu->reg[regB];
     break;
-
-  case ALU_INC:
-    cpu->reg[regA] += 1;
-    break;
-
-  case ALU_DEC:
-    cpu->reg[regA] -= 1;
-    break;
-    // TODO: implement more ALU ops
   }
 }
 
@@ -89,7 +127,8 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
  */
 void cpu_run(struct cpu *cpu)
 {
-  int running = 1; // True until we get a HLT instruction
+  int running = 1;    // True until we get a HLT instruction
+  cpu->reg[7] = 0xF4; //cpu_load
 
   while (running)
   {
@@ -111,19 +150,41 @@ void cpu_run(struct cpu *cpu)
       // cpu->pc += 3;
       break;
 
-      // void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
-    case MUL:
-      alu(cpu, ALU_MUL, operandA, operandB);
-      // cpu->pc += 3;
-      break;
-
     case PRN:
       printf("%d\n", cpu->reg[operandA]);
-      // cpu->pc += 2;
       break;
 
     case HLT:
       running = 0;
+      break;
+
+    case MUL:
+      alu(cpu, ALU_MUL, operandA, operandB);
+      break;
+
+    case ADD:
+      alu(cpu, ALU_ADD, operandA, operandB);
+      break;
+
+    case DIV:
+      alu(cpu, ALU_DIV, operandA, operandB);
+      break;
+
+    case SUB:
+      alu(cpu, ALU_SUB, operandA, operandB);
+      break;
+
+    case PUSH:
+      // printf("I am in push");
+      cpu->reg[7]--;
+      cpu_ram_write(cpu, cpu->reg[7], cpu->reg[operandA]);
+      break;
+
+    case POP:
+      printf("I am in POP");
+      // printf("%s POP\n", cpu->reg[operandA]);
+      cpu->reg[operandA] = cpu_ram_read(cpu, cpu->reg[7]);
+      cpu->reg[7]++;
       break;
 
     default:
@@ -135,8 +196,7 @@ void cpu_run(struct cpu *cpu)
 
     // 4. Move the PC to the next instruction.
     //PC + 1 or PC + 2
-    const int move = (IR >> 6) + 1;
-    cpu->pc += move;
+    cpu->pc += (IR >> 6) + 1;
   }
 }
 
@@ -146,6 +206,12 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
-  cpu->pc = 0;
+  // cpu->pc = 0;
   // TODO: Zero registers and RAM
+  // for(int i = 0; i< si)
+  cpu->reg[7] = 0xF4; //cpu_load
+  memset(cpu->reg, 0, sizeof(cpu->reg));
+
+  memset(cpu->ram, 0, sizeof(cpu->ram));
+  // printf("I am here CPU_INIT \n");
 }
--- ls8/cpu.h
+++ ls8/cpu.h
@@ -19,8 +19,8 @@ enum alu_op
   ALU_MUL,
   ALU_ADD,
   ALU_SUB,
-  ALU_INC,
-  ALU_DEC,
+  // ALU_INC,
+  // ALU_DEC,
   ALU_DIV,
 };
 // Instructions
@@ -32,11 +32,20 @@ enum alu_op
 #define PRN 0b01000111
 #define HLT 0b00000001
 #define MUL 0b10100010
+#define ADD 0b10110011
+#define DIV 0b10101011
+#define SUB 0b10101001
+// #define INC 0b01111000
+// #define DEC 0b01111001
+#define PUSH 0b01000101
+#define POP 0b01000110
+
 // TODO: more instructions here. These can be used in cpu_run().
 
 // Function declarations
 
-extern void cpu_load(struct cpu *cpu);
+extern void cpu_load(struct cpu *cpu, char *argv[]);
+// extern void cpu_load(struct cpu *cpu);
 extern void cpu_init(struct cpu *cpu);
 extern void cpu_run(struct cpu *cpu);
 
