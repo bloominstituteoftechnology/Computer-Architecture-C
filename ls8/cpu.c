66f92905e999673934be51b73ce8317ae5883fbb
derrick-mei <derrick.mei.168@gmail.com>
2018-08-21 21:32:09 -0500

    finished step 7: unhardcode cpu_load

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -7,29 +7,6 @@
  * THIS IS WHAT WILL HAPPEN LATER
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu, char *filename)
-{
-    const int DATA_LEN = 6;
-    char data[6] = {
-        // From print8.ls8
-        0b10000010, // LDI R0,8
-        0b00000000,
-        0b00001000,
-        0b01000111, // PRN R0
-        0b00000000,
-        0b00000001 // HLT
-    };
-
-    int address = 0;
-
-    // PUTTING THAT STUFF ABOVE INTO RAM
-    for (int i = 0; i < DATA_LEN; i++)
-    {
-        cpu->ram[address++] = data[i];
-    }
-
-    // TODO: Replace this with something less hard-coded
-}
 
 // STEP 2
 unsigned char cpu_ram_read(struct cpu *cpu, int index)
@@ -41,6 +18,68 @@ unsigned char cpu_ram_read(struct cpu *cpu, int index)
 unsigned char cpu_ram_write(struct cpu *cpu, unsigned char address, unsigned char value)
 {
     return cpu->ram[address] = value;
+}
+
+void cpu_load(struct cpu *cpu, char *filename)
+{
+    // const int DATA_LEN = 6;
+    // char data[6] = {
+    //     // From print8.ls8
+    //     0b10000010, // LDI R0,8
+    //     0b00000000,
+    //     0b00001000,
+    //     0b01000111, // PRN R0
+    //     0b00000000,
+    //     0b00000001 // HLT
+    // };
+
+    // int address = 0;
+
+    // // PUTTING THAT STUFF ABOVE INTO RAM
+    // for (int i = 0; i < DATA_LEN; i++)
+    // {
+    //     cpu->ram[address++] = data[i];
+    // }
+
+    // STEP 7.2: TODO: Replace this with something less hard-coded
+    // 0-255
+    unsigned char address = 0;
+    // variable to hold the line
+    char line[256];
+    // pointer to hold file
+    FILE *fp;
+
+    // open file
+    fp = fopen(filename, "r");
+    if (fp == NULL) // error handling for opening file
+    {
+        fprintf(stderr, "Unable to open file: %s\n", filename);
+        exit(3);
+    }
+
+    // fgets gets the line and sets it to our variable "line" and then moves the pointer to the next line
+    while (fgets(line, sizeof line, fp) != NULL)
+    {
+        //printf("%s\n", line);
+        // variable for the first non number from strtoul
+        unsigned char *end;
+        // variable for the output of strtoul()
+        unsigned char b_number;
+
+        // strtoul calculates the value in what base(2) we want into b_number
+        // it assigns the first non-number in the "line" to "end"
+        b_number = strtoul(line, &end, 2);
+
+        // if line is empty aka "\n" then line and end are equal
+        if (line == end)
+        {
+            continue;
+        }
+
+        cpu_ram_write(cpu, address++, b_number);
+    }
+
+    //fclose(fp);
 }
 
 /**
@@ -92,10 +131,10 @@ void cpu_run(struct cpu *cpu)
         switch (numArgs)
         {
         case 3:
-        // printf("2: MDR\n");
+            // printf("2: MDR\n");
             MDR = cpu_ram_read(cpu, PC + 2);
         case 2:
-        // printf("2.5: MAR\n");
+            // printf("2.5: MAR\n");
             MAR = cpu_ram_read(cpu, PC + 1);
             break;
         }
