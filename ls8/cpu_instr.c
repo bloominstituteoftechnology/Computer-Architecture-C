781d241c9a57ff4721b4838fe93b31b54674ae8a
Danny Vail <danny_vail@outlook.com>
2018-08-22 04:45:38 -0400

    IMPLEMENT PUSH AND POP INSTRUCTION HANDLERS In cpu_instr.c - Implements handle_PUSH() and handle_POP() to push and pop onto the memory stack - Adds push and pop functions to branch_table

--- ls8/cpu_instr.c
+++ ls8/cpu_instr.c
@@ -38,6 +38,18 @@ void handle_MUL(struct cpu *cpu, unsigned char opA, unsigned char opB)
 }
 
 /**
+ * CPU Instruction: Pop first value out of stack and store in regA
+ * 
+ * @param cpu {struct cpu*} Pointer to a cpu struct.
+ * @param opA {unsigned char} Operand A: register.
+ * @param opB {unsigned char} Operand B: N/A
+ */
+void handle_POP(struct cpu *cpu, unsigned char opA, unsigned char opB){
+  cpu->registers[opA] = cpu->ram[cpu->registers[SP]];
+  cpu->registers[SP] += 1;
+}
+
+/**
  * CPU Instruction: Prints numeric value stored in given register
  * 
  * @param cpu {struct cpu*} Pointer to a cpu struct.
@@ -50,6 +62,19 @@ void handle_PRN(struct cpu *cpu, unsigned char opA, unsigned char opB)
 }
 
 /**
+ * CPU Instruction: Push given value on to stack
+ * 
+ * @param cpu {struct cpu*} Pointer to a cpu struct.
+ * @param opA {unsigned char} Operand A: register.
+ * @param opB {unsigned char} Operand B: N/A
+ */
+void handle_PUSH(struct cpu *cpu, unsigned char opA, unsigned char opB)
+{
+  cpu->registers[SP] -= 1;
+  cpu->ram[cpu->registers[SP]] = cpu->registers[opA];
+}
+
+/**
  * Loads CPU instructions into branch table
  * 
  * @param bt {handler*} Pointer to an array of function pointers.
@@ -58,5 +83,7 @@ void load_cpu_instructions(handler *bt)
 {
   bt[LDI] = handle_LDI;
   bt[MUL] = handle_MUL;
+  bt[POP] = handle_POP;
   bt[PRN] = handle_PRN;
+  bt[PUSH] = handle_PUSH;
 }
