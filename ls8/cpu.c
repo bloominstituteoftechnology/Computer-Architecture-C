1df13eedf60d7fba9f7de3960d562c49cb002aa4
mister-corn <code@ronaldlibago.com>
2018-08-21 18:24:54 -0700

    18:24 PDT 21-Aug: printstr.ls8 is working. LS8 says, "Hello!"

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -19,49 +19,34 @@ void throwHandsUpAndGiveUp(void)
 void cpu_load(struct cpu *cpu, char *argv[])
 {
   FILE *fp = fopen(argv[1], "r");
-  char *strBuff = 0;
-  char *token;
-  long length;
-  int address;
+  char line[255];
+  int address = 0;
 
   printf("\n"); // print a new line so I better distinguish output in terminal
 
-  /* Open file and copy to string buffer strBuff */
-  // Shamelessly copied from 
-  // https://stackoverflow.com/questions/174531/how-to-read-the-content-of-a-file-to-a-string-in-c
-  if (  !fp  )                          throwHandsUpAndGiveUp(); // Error if FP is null
-
-  if (  fseek(fp, 0, SEEK_END)   != 0)  throwHandsUpAndGiveUp(); 
-        length = ftell(fp);
-  if (  length == -1 )                  throwHandsUpAndGiveUp();
-  if (  fseek(fp, 0, SEEK_SET)   != 0)  throwHandsUpAndGiveUp();
-
-  strBuff = malloc(length + 1);
-
-  if (strBuff) fread(strBuff, 1, length, fp);
-
-  fclose(fp);
-  strBuff[length] = '\0';
-
-  /* Tokenize and parse string in strBuff */
-  if (strBuff)
+  while(fgets(line, sizeof(line), fp) != NULL)
   {
-    // start to process your data / extract strings here...
-    token = strtok(strBuff, " \r\n");
-    address = 0;
-
-    while (token != NULL) {
-      if (token[0] == '0' || token[0] == '1') {
-        unsigned char numberedToken = strtoul(token, NULL, 2);
-        #if DEBUG
-        // Print instructions being written to memory
-        printf("token at address %i:\t%s | HEX: %02x\n", address, token,numberedToken);
-        #endif
-        cpu->ram[address++] = numberedToken;
-      }
-      token = strtok(NULL, " \r\n");
+    line[9] = '\0';
+    if (line[0] == '0' || line[0] == '1') {
+      #if DEBUG
+      printf("WRITTEN TO ADDR %i: \t%s\n", address, line);
+      #endif
+      cpu->ram[address++] = strtoul(line, NULL, 2);
     }
   }
+
+  #if DEBUG
+  // Print what is currently saved to memory
+  printf("\n---Current Memory State---\n");
+  int length = address;
+  int current = 0;
+
+  while (current <= length) {
+    printf("RAM ADDR %i:\t%02x\n", current, cpu->ram[current]);
+    current++;
+  }
+  printf("\n");
+  #endif
 }
 
 /**
@@ -69,20 +54,24 @@ void cpu_load(struct cpu *cpu, char *argv[])
  */
 void alu(struct cpu *cpu, enum alu_op op)
 {
-  int regA;
-  int regB;
+  int regA = cpu_ram_read(cpu, cpu->PC + 1);
+  int regB = cpu_ram_read(cpu, cpu->PC + 2);
 
   switch (op) {
     case ALU_ADD:
-      regA = cpu_ram_read(cpu, cpu->PC + 1);
-      regB = cpu_ram_read(cpu, cpu->PC + 2);
       handleALU_ADD(cpu, regA, regB);
       break;
     case ALU_MUL:
-      // TODO
-      regA = cpu_ram_read(cpu, cpu->PC + 1);
-      regB = cpu_ram_read(cpu, cpu->PC + 2);
       handleALU_MUL(cpu, regA, regB);
+      break;
+    case ALU_INC:
+      handleALU_INC(cpu, regA);
+      break;
+    case ALU_DEC:
+      handleALU_DEC(cpu, regA);
+      break;
+    case ALU_CMP:
+      handleALU_CMP(cpu, regA, regB);
       break;
 
     // TODO: implement more ALU ops
@@ -107,17 +96,32 @@ void cpu_run(struct cpu *cpu)
     IR = cpu_ram_read(cpu, cpu->PC);
 
     switch (IR) {
+      case LD:
+        handleLD(cpu);
+        break;
       case LDI:
         handleLDI(cpu);
         break;
       case PRN:
         handlePRN(cpu);
         break;
+      case PRA:
+        handlePRA(cpu);
+        break;
       case ADD:
         alu(cpu, ALU_ADD);
         break;
       case MUL:
         alu(cpu, ALU_MUL);
+        break;
+      case INC:
+        alu(cpu, ALU_INC);
+        break;
+      case DEC:
+        alu(cpu, ALU_DEC);
+        break;
+      case CMP:
+        alu(cpu, ALU_CMP);
         break;
       case PUSH:
         handlePUSH(cpu);
@@ -131,24 +135,34 @@ void cpu_run(struct cpu *cpu)
       case RET:
         handleRET(cpu);
         break;
+      case JMP:
+        handleJMP(cpu);
+        break;
+      case JEQ:
+        handleJEQ(cpu);
+        break;
       case HLT:
         running = 0;
         break;
       default:
-        fprintf(stderr, "Unkown instruction at %i: %02x\n", cpu->PC, IR);
+        fprintf(stderr, "Unknown instruction at %i: %02x\n", cpu->PC, IR);
         exit(2);
     }
   }
 
   #if DEBUG
+  printf("\n");
   // Print first 16 positions of the stack in memory
-  for (int q = 0xF3; q > (0xF3 - 0x10); q--) {
+  for (int q = 0xF3; q > (0xF3 - 0x08); q--) {
     printf("stack @ addr:%i: %i\n", q, cpu_ram_read(cpu, q));
   }
   // Print current stack pointer
   // At 0th place, SP should be 0xF4/244;
   printf("current stack pointer: %i\n", cpu->SP);
   #endif
+
+  free(cpu->registers);
+  free(cpu->ram);
 }
 
 /**
@@ -157,8 +171,9 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
-  cpu->PC = 0;
-  cpu->SP = 0xF4;
+  cpu->PC = 0;            // Program counter
+  cpu->SP = 0xF4;         // Stack pointer
+  cpu->FL = 0b00000000;   // Flags (for CMP)
   // TODO: Zero registers and RAM
   cpu->registers = (unsigned char *) calloc(8, sizeof(unsigned char));
   cpu->ram = (unsigned char *) calloc(256, sizeof(unsigned char));
