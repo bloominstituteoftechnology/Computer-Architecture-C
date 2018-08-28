0ca4a3668550c3ad8e906836d88f2c1d7267d9b7
Herbert Hodgson <herberthodgson@hotmail.com>
2018-08-28 19:48:04 -0400

    Implemented the HLT, LDI, PRN, MUL, and ADD instruction handlers. Un-hardcoded the machine code and implemented cpu_run(). print8 and mult examples running.

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,28 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "cpu.h"
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(char *filename, struct cpu *cpu)
 {
-  const int DATA_LEN = 6;
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+  FILE *fp;
+  char line[256];
+  int address = ADDR_PROGRAM_ENTRY;
 
-  int address = 0;
-
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  // Open the source file
+  if ((fp = fopen(filename, "r")) == NULL)
+  {
+    fprintf(stderr, "Cannot open file %s\n", filename);
+    exit(2);
   }
 
-  // TODO: Replace this with something less hard-coded
+  // Read all the lines and store them in RAM
+  while (fgets(line, sizeof line, fp) != NULL)
+  {
+
+    // Convert string to a number
+    char *endchar;
+    unsigned char byte = strtol(line, &endchar, 2);
+    ;
+
+    // Ignore lines from whicn no numbers were read
+    if (endchar == line)
+    {
+      continue;
+    }
+
+    // Store in ram
+    cpu->ram[address++] = byte;
+  }
 }
 
 /**
@@ -30,12 +44,19 @@ void cpu_load(struct cpu *cpu)
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
-  switch (op) {
-    case ALU_MUL:
-      // TODO
-      break;
+  unsigned char *reg = cpu->reg;
 
-    // TODO: implement more ALU ops
+  unsigned char valB = reg[regB];
+
+  switch (op)
+  {
+  case ALU_MUL:
+    reg[regA] *= valB;
+    break;
+
+  case ALU_ADD:
+    reg[regA] += valB;
+    break;
   }
 }
 
@@ -44,14 +65,60 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
  */
 void cpu_run(struct cpu *cpu)
 {
+  // Just so we don't have to type cpu-> every time
+  unsigned char *reg = cpu->reg;
+  unsigned char *ram = cpu->ram;
+
   int running = 1; // True until we get a HLT instruction
 
-  while (running) {
-    // TODO
-    // 1. Get the value of the current instruction (in address PC).
-    // 2. switch() over it to decide on a course of action.
-    // 3. Do whatever the instruction should do according to the spec.
-    // 4. Move the PC to the next instruction.
+  while (running)
+  {
+    // Get the value of the current instruction (in address PC)
+    unsigned char IR = ram[cpu->PC];
+
+    unsigned char operandA = ram[(cpu->PC + 1) & 0xff];
+    unsigned char operandB = ram[(cpu->PC + 2) & 0xff];
+
+    // True if this instruction might set the PC
+    int instruction_set_pc = (IR >> 4) & 1;
+
+    // switch() over it to decide on a course of action
+    switch (IR)
+    {
+
+    case LDI:
+      reg[operandA] = operandB;
+      break;
+
+    case PRN:
+      printf("%d\n", reg[operandA]);
+      break;
+
+    case MUL:
+      alu(cpu, ALU_MUL, operandA, operandB);
+      break;
+
+    case ADD:
+      alu(cpu, ALU_ADD, operandA, operandB);
+      break;
+
+    case HLT:
+      running = 0;
+      break;
+
+    case PRA:
+      printf("%c\n", reg[operandA]);
+      break;
+
+    default:
+      fprintf(stderr, "PC %02x: unknown instruction %02x\n", cpu->PC, IR);
+      exit(3);
+    }
+
+    if (!instruction_set_pc)
+    {
+      cpu->PC += ((IR >> 6) & 0x3) + 1;
+    }
   }
 }
 
@@ -60,7 +127,9 @@ void cpu_run(struct cpu *cpu)
  */
 void cpu_init(struct cpu *cpu)
 {
-  // TODO: Initialize the PC and other special registers
+  cpu->PC = 0;
 
-  // TODO: Zero registers and RAM
+  // Zero registers and RAM
+  memset(cpu->reg, 0, sizeof cpu->reg); // filling the reg array defined in cpu.h with zeros
+  memset(cpu->ram, 0, sizeof cpu->ram); // filling the ram array defined in cpu.h with zeros
 }
--- ls8/cpu.h
+++ ls8/cpu.h
@@ -4,22 +4,39 @@
 // Holds all information about the CPU
 struct cpu // implement step 1 so that this struct can be used in cpu.c
 {
-  unsigned char PC;
-  unsigned char registers[8]; // allocate memory to registers
-  unsigned char ram[256];     // allocate 256 bytes of memory to ram
+  unsigned char PC;       // program counter
+  unsigned char reg[8];   // allocate memory to registers
+  unsigned char ram[256]; // allocate 256 bytes of memory to ram
 };
+
+// Special register values
+#define SP 5
+
+// ALU operations
+enum alu_op
+{
+  ALU_MUL,
+  ALU_ADD
+};
+
+// Memory locations
+#define ADDR_PROGRAM_ENTRY 0x00 // Where programs start getting loaded
 
 // Instructions
 
 // These use binary literals. If these aren't available with your compiler, hex
 // literals should be used.
 
+#define ADD 0b10100000
+#define HLT 0b00000001
 #define LDI 0b10000010
-// TODO: more instructions here. These can be used in cpu_run().
+#define MUL 0b10100010
+#define PRA 0b01001000
+#define PRN 0b01000111
 
 // Function declarations
 
-extern void cpu_load(struct cpu *cpu);
+extern void cpu_load(char *filename, struct cpu *cpu);
 extern void cpu_init(struct cpu *cpu);
 extern void cpu_run(struct cpu *cpu);
 
