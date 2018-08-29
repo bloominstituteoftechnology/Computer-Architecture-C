be77622f7ae5a1cf77d3d53f8fb90bbd51e0a39e
Tramane Hall <tramanedhall@gmail.com>
2018-08-29 15:17:09 -0400

    finished ALU instructions and ls8.c

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -4,12 +4,16 @@
 /**
  * Main
  */
-int main(void)
+int main(int argc, char *argv[])
 {
   struct cpu cpu;
+  char *file = "examples/print8.ls8";
+  if (argc == 2) {
+    file == argv[1];
+  }
 
   cpu_init(&cpu);
-  cpu_load(&cpu);
+  cpu_load(&cpu, file);
   cpu_run(&cpu);
 
   return 0;
