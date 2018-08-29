a105157364a192f3c16f2d14a184d76b10b34220
Tramane Hall <tramanedhall@gmail.com>
2018-08-29 14:00:08 -0400

    finished ALU, implementing more ALU ops WIP

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -55,7 +55,8 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 {
   switch (op) {
     case ALU_MUL:
-      // TODO
+      cpu->reg[regA] = cpu->reg[regA] * cpu->reg[regB];
+      cpu->PC += 3;
       break;
 
     // TODO: implement more ALU ops
