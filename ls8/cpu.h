f52f1d54f92558e7f8d7c0595c1e2c4d67d97225
april <martinezapril56@yahoo.com>
2018-08-30 03:10:45 -0400

    working on cpu.h

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -7,12 +7,24 @@ struct cpu {
  unsigned char reg[8];//creates an array with 8 slots in it
  unsigned char ram[256];// hold the rest of the memory
 };
+// specail register Values
+#define SP 5
+//ALU operations
+enum alu_op{
+    ALU_MUL,
+    ALU_ADD
+};
+//Memory locations
+#define ADDR_PROGRAM_ENTRY 0x00//where program start getting loaded
+#define ADDR_EMPTY_STACK 0xF4//where SP is in an empty stack
 
 // Instructions
 
 // These use binary literals. If these aren't available with your compiler, hex
 // literals should be used.
-
+#define ADD  0b10100000
+#define CALL 0b01010000
+#define HLT  0b00000001
 #define LDI  0b10000010
 #define PRN  0b10000111
 #define HLT  0b00000001
