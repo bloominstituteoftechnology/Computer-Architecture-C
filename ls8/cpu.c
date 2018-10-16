ebc83af72331406d45af46355c5b6760a18dc3c5
Alec <afroenygam@gmail.com>
2018-10-16 18:24:08 -0400

    minor update

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -20,21 +20,6 @@ void cpu_ram_write()
 }
 void cpu_load(char *filename, struct cpu *cpu)
 {
-  // char data[DATA_LEN] = {
-  //   // From print8.ls8
-  //   0b10000010, // LDI R0,8
-  //   0b00000000,
-  //   0b00001000,
-  //   0b01000111, // PRN R0
-  //   0b00000000,
-  //   0b00000001  // HLT
-  // };
-
-  // int address = 0;
-
-  // for (int i = 0; i < DATA_LEN; i++) {
-  //   cpu->ram[address++] = data[i];
-
   FILE *fp;
   char line[1024];
   int address = ADDR_PROGRAM_ENTRY;
@@ -128,10 +113,9 @@ void cpu_run(struct cpu *cpu)
  */
 void cpu_init(struct cpu *cpu)
 {
-  cpu->PC = 0;
   // TODO: Initialize the PC and other special registers
-
-  // TODO: Zero registers and RAM
+  cpu->PC = 0;
+  // Zero registers and RAM
   memset(cpu->reg, 0, sizeof cpu->reg);
   memset(cpu->ram, 0, sizeof cpu->ram);
 }
