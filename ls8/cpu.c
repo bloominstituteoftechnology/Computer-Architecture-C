5be32e7a89b95d1f7f565825d5190153320270ce
Vu Cao <cdhvu@yahoo.com>
2018-10-16 15:35:36 -0700

    Added ram function cpu_ram_read and cpu_ram_write

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -3,23 +3,37 @@
 #define DATA_LEN 6
 
 /**
+ * cpu_ram_read/cpu_ram_write
+ */
+unsigned char cpu_ram_read(struct cpu *cpu, int idx)
+{
+  return cpu->ram[idx];
+}
+
+unsigned char cpu_ram_write(struct cpu *cpu, int idx, unsigned char val)
+{
+  cpu->ram[idx] = val;
+}
+
+/**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
 void cpu_load(struct cpu *cpu)
 {
   char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
+      // From print8.ls8
+      0b10000010, // LDI R0,8
+      0b00000000,
+      0b00001000,
+      0b01000111, // PRN R0
+      0b00000000,
+      0b00000001 // HLT
   };
 
   int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
+  for (int i = 0; i < DATA_LEN; i++)
+  {
     cpu->ram[address++] = data[i];
   }
 
@@ -31,10 +45,11 @@ void cpu_load(struct cpu *cpu)
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
-  switch (op) {
-    case ALU_MUL:
-      // TODO
-      break;
+  switch (op)
+  {
+  case ALU_MUL:
+    // TODO
+    break;
 
     // TODO: implement more ALU ops
   }
@@ -47,7 +62,8 @@ void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
-  while (running) {
+  while (running)
+  {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
     // 2. switch() over it to decide on a course of action.
