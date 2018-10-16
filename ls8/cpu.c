956b56b75c97feeed5e3d4fe40009695d3539a03
JJ Ashcraft <jj@jjashcraft.com>
2018-10-16 15:58:05 -0400

    ram_read and ram_write complete

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,30 +1,34 @@
 #include "cpu.h"
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define DATA_LEN 6
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-FILE *fileptr;
-char *filename = "./examples/print8.ls8";
-char *newdata;
 
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address)
+{
+  return cpu->ram[address];
+}
+
+unsigned char cpu_ram_write(struct cpu *cpu, unsigned char address, unsigned char value)
+{
+  return cpu->ram[address] = value;
+}
 
 void cpu_load(struct cpu *cpu)
 {
-  // fileptr = fopen(filename, "r");
-  // if (filename==NULL) {
-  //   printf("error opening file %s\n", filename);
-  // } else {
-  //   printf("file opened successfully.");
-  //   int i = 0;
-  //   while (fgets(newdata, 300, fileptr)) {
-  //   i++;
-  //   printf("%s\n", newdata);
-  //   }
-  // }
+  FILE *fileptr;
+  // char filename = "./examples/print8.ls8";
+  char newdata[1024];
+  fileptr = fopen("print8.ls8", "r");
+    while (fgets(newdata, sizeof newdata, fileptr) != NULL) {
+    printf("%s\n", newdata);
+    }
+
 
   char data[DATA_LEN] = {
     // From print8.ls8
@@ -41,21 +45,11 @@ void cpu_load(struct cpu *cpu)
 
   for (int i = 0; i < DATA_LEN; i++) {
     cpu->ram[address++] = data[i];
-    printf("%s", cpu->ram);
   }
 
   // TODO: Replace this with something less hard-coded
 }
 
-void cpu_ram_write(struct cpu *cpu)
-{
-  *cpu->ram[0] = "tested";
-}
-
-void cpu_ram_read(struct cpu *cpu)
-{
-  printf("%d\n", *cpu->ram[0]);
-}
 
 
 
@@ -79,8 +73,31 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
-
+  unsigned char IR, operandA, operandB;
+  
   while (running) {
+    IR = cpu_ram_read(cpu, cpu->pc);
+    operandA = cpu_ram_read(cpu, cpu->pc+1);
+    operandB = cpu_ram_read(cpu, cpu->pc+2);
+    printf("TRACE: %02X: %02X %02X %02X\n", cpu->pc, IR, operandA, operandB);
+    switch(IR) {
+      case LDI:
+        cpu_ram_write(cpu, operandA, operandB);
+        printf("writing to ram");
+        break;
+
+      case PRN:
+        printf("%d\n", cpu_ram_read(cpu, operandA));
+        break;
+
+      case HLT:
+        printf("stopping");
+        running = 0;
+        break;
+
+      default:
+      break;
+    }
     // TODO
     // 1. Get the value of the current instruction (in address PC).
     // 2. switch() over it to decide on a course of action.
@@ -97,5 +114,8 @@ void cpu_init(struct cpu *cpu)
   // TODO: Initialize the PC and other special registers
 
   // TODO: Zero registers and RAM
+  cpu->pc = 0;
 
+  memset(cpu->ram, 0, sizeof cpu-> ram);
+  memset(cpu->registers, 0, sizeof cpu->registers);
 }
--- ls8/cpu.h
+++ ls8/cpu.h
@@ -7,9 +7,9 @@ struct cpu {
   // PC
 unsigned char *pc;
   // registers (array)
-unsigned char *registers[64];
+unsigned char *registers[8];
 // ram (array)
-unsigned char *ram[1000];
+unsigned char *ram[256];
 };
 
 // ALU operations
@@ -24,6 +24,8 @@ enum alu_op {
 // literals should be used.
 
 #define LDI  0b10000010
+#define PRN 0b01000111
+#define HLT 0b00000001
 // TODO: more instructions here. These can be used in cpu_run().
 
 // Function declarations
