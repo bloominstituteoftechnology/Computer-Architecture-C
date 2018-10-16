6e68f25f36a9366630daca2527780a190149b800
snowcoding <vimalshah77@gmail.com>
2018-10-16 12:59:10 -0700

    Comment on Starting main

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include "cpu.h"
+#include <stdio.h>
 
 /**
  * Main
  */
 int main(void)
 {
+  printf("Starting Main....\n");
+  
+  //Instantiate the cpu structure:
   struct cpu cpu;
 
+  //Call the initialize function on address of cpu:
   cpu_init(&cpu);
   cpu_load(&cpu);
   cpu_run(&cpu);
