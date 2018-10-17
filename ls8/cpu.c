c9ec25b0291602ae517d87201338629e41ee2cfa
Kevin Tena <kevten09@gmail.com>
2018-10-17 17:26:50 -0500

    Implemented MUL

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -8,23 +8,35 @@
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(char *filename, struct cpu *cpu)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+  FILE *fp;
+  char line[1024];
+  unsigned char address = 0x00;
 
-  int address = 0;
+  fp = fopen(filename, "r");
 
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  if(fp == NULL) {
+    fprintf(stderr, "Error opening file %s\n", filename);
+    exit(2);
   }
+
+  while (fgets(line, sizeof line, fp) != NULL) {
+		char *endptr = NULL;
+
+    unsigned char b = strtoul(line, &endptr, 2);
+
+		if (endptr == line) {
+			printf("Ignoring this line.\n");
+			continue;
+		}
+
+    cpu_ram_write(cpu, address++, b);
+	}
+
+	fclose(fp);
+
+
 
   // TODO: Replace this with something less hard-coded
 }
@@ -36,11 +48,21 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 {
   switch (op) {
     case ALU_MUL:
-      // TODO
+      cpu->reg[regA] *= cpu->reg[regB];
       break;
 
     // TODO: implement more ALU ops
   }
+}
+
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address)
+{
+  return cpu->ram[address];
+}
+
+void cpu_ram_write(struct cpu *cpu, unsigned char address, unsigned char value)
+{
+  cpu->ram[address] = value;
 }
 
 /**
@@ -70,6 +92,9 @@ void cpu_run(struct cpu *cpu)
       case HLT:
         running = 0;
         break;
+      case MUL:
+        alu(cpu, ALU_MUL, operandA, operandB);
+        break;
       default:
         fprintf(stderr, "PC %02x: unknown instruction %02x\n", cpu->PC, IR);
         exit(3);
@@ -91,16 +116,11 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
+  cpu->PC = 0;
 
   // TODO: Zero registers and RAM
+  memset(cpu->reg, 0, sizeof cpu->reg);
+  memset(cpu->ram, 0, sizeof cpu->ram);
+
 }
 
-unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address)
-{
-  return cpu->ram[address];
-}
-
-void cpu_ram_write(struct cpu *cpu, unsigned char address, unsigned char value)
-{
-  cpu->ram[address] = value;
-}
