fe25fb436cc525ddefc873160fdde93a37de63b0
emaquino44 <prexy44@yahoo.com>
2018-10-16 19:14:03 -0700

    Completed Un-hardcode the machine code

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,6 +1,7 @@
 #include "cpu.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define DATA_LEN 6
 
@@ -17,25 +18,38 @@ void cpu_ram_write(struct cpu *cpu, unsigned char address, unsigned char value) 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char *filename)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+  // char data[DATA_LEN] = {
+  //   // From print8.ls8
+  //   0b10000010, // LDI R0,8
+  //   0b00000000,
+  //   0b00001000,
+  //   0b01000111, // PRN R0
+  //   0b00000000,
+  //   0b00000001  // HLT
+  // };
 
-  int address = 0;
+  // int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
-  }
+  // for (int i = 0; i < DATA_LEN; i++) {
+  //   cpu->ram[address++] = data[i];
+  // }
 
-  // TODO: Replace this with something less hard-coded
+    // TODO: Replace this with something less hard-coded
+  FILE *fp;
+    char line[256];
+    int counter = 0;
+    char *ptr;
+
+    if ((fp = fopen(filename, "r")) == NULL) {
+      fprintf(stderr, "This files does not exist");
+      exit(1);
+    }
+      while (fgets(line, sizeof(line), fp) != NULL) {
+        cpu->ram[counter++] = strtoul(line, &ptr, 2);
+    }
+    fclose(fp);
 }
 
 /**
--- ls8/cpu.h
+++ ls8/cpu.h
@@ -29,7 +29,7 @@ enum alu_op {
 
 // Function declarations
 
-extern void cpu_load(struct cpu *cpu);
+extern void cpu_load(struct cpu *cpu, char *filename);
 extern void cpu_init(struct cpu *cpu);
 extern void cpu_run(struct cpu *cpu);
 
