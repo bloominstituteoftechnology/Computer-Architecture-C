1f02b6dadff41a2ab1b2aefd5f57cda564958115
snowcoding <vimalshah77@gmail.com>
2018-10-17 14:06:12 -0700

    Reorder code

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -8,15 +8,15 @@
 int main(int argc, char **argv)
 {
   printf("\nStarting Main....\n");
-  
-  if (argc != 2){
-    fprintf(stderr, "usage: ls8 filename.ls8");
-    return 1;
-  }
-  
+    
   //Instantiate the cpu structure:
   struct cpu cpu;
 
+  if (argc != 2){
+    fprintf(stderr, "usage: ls8 filename.ls8\n");
+    return 1;
+  }
+  
   //Call the initialize function on address of cpu:
   cpu_init(&cpu);
   cpu_load(&cpu, argv[1]); 
