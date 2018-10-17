2fb0ea72b0ec830851b6d8b4c32a34470504dec7
Lonnie Davis <ldavis0866@gmail.com>
2018-10-17 14:59:09 -0500

    mult.ls8 working

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,45 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
+//#include <string.h>
 #include "cpu.h"
 
 
 #define DATA_LEN 6
 
 
-unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address)
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char MAR)
 {
-  return cpu->ram[address];
+  return cpu->ram[MAR];
 }
 
-void cpu_ram_write(struct cpu *cpu, unsigned char address, unsigned char value)
+void cpu_ram_write(struct cpu *cpu, unsigned char MAR, unsigned char MDR)
 {
- cpu->ram[address] = value;
+ cpu->ram[MAR] = MDR;
 }
 
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char *filename)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
-
+  char line[1024];
   int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  FILE *fp = fopen(filename, "r");
+  //printf("%s\n", filename);
+  if (fp == NULL) {
+    fprintf(stderr, "error opening file %s\n", filename);
+    exit(2);
   }
 
-  // TODO: Replace this with something less hard-coded
+  while(fgets(line, sizeof line, fp) != NULL) {
+    //puts(line);
+    char *endchar;
+    unsigned char data = strtoul(line, &endchar, 2);
+
+    if (line == endchar) {
+      continue;
+    }
+    //printf("%u\n", data);
+    cpu_ram_write(cpu, address++, data);
+  }
+  fclose(fp);
 }
+
+
+//   char data[DATA_LEN] = {
+//     // From print8.ls8
+//     0b10000010, // LDI R0,8
+//     0b00000000,
+//     0b00001000,
+//     0b01000111, // PRN R0
+//     0b00000000,
+//     0b00000001  // HLT
+//   };
+
+//   //int address = 0;
+
+//   for (int i = 0; i < DATA_LEN; i++) {
+//     cpu->ram[address++] = data[i];
+//   }
+
+//   // TODO: Replace this with something less hard-coded
+// }
 
 /**
  * ALU
@@ -48,13 +74,13 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 {
   switch (op) {
     case ALU_MUL:
-      // TODO
+      cpu->reg[regA] *= cpu->reg[regB];
       break;
 
     // TODO: implement more ALU ops
     case ALU_ADD:
 
-    break;
+      break;
   }
 }
 
@@ -75,24 +101,28 @@ void cpu_run(struct cpu *cpu)
     operandA = cpu_ram_read(cpu, cpu->pc + 1);
     operandB = cpu_ram_read(cpu, cpu->pc + 2);
 
-    int add_to_pc = (IR >> 6) + 1;
+    //int add_to_pc = (IR >> 6) + 1;
 
-    //printf("TRACE: %02x: %02x\n", cpu->pc, IR);
-    printf("TRACE: %02X: %02X %02X %02X\n", cpu->pc, IR, operandA, operandB);
+    printf("TRACE: %02x: %02x\n", cpu->pc, IR);
+    //printf("TRACE: %02X: %02X %02X %02X\n", cpu->pc, IR, operandA, operandB);
     // 2. switch() over it to decide on a course of action.
     switch(IR) {
 
       case LDI:
-        cpu->registers[operandA] = operandB;
+        cpu->reg[operandA] = operandB;
         cpu->pc += 3;
         break;
       case PRN:
-        printf("%d\n", cpu->registers[operandA]);
+        printf("%d\n", cpu->reg[operandA]);
         cpu->pc += 2;
         break;
       case HLT:
         running = 0;
         cpu->pc += 1;
+        break;
+      case MUL:
+        alu(cpu, ALU_MUL, operandA, operandB);
+        cpu->pc += 3;
         break;
       default:
         printf("unknown instruction at %02x: %02x\n", cpu->pc, IR);
@@ -112,8 +142,8 @@ void cpu_init(struct cpu *cpu)
   cpu->pc = 0;
 
   // TODO: Zero registers and RAM
-  memset(cpu->ram, 0, sizeof cpu->ram);
-  memset(cpu->registers, 0, sizeof cpu->registers);
+  //memset(cpu->ram, 0, sizeof cpu->ram);
+  //memset(cpu->registers, 0, sizeof cpu->registers);
 
   //cpu->registers[7] = 0xF4;
 }
--- ls8/cpu.h
+++ ls8/cpu.h
@@ -8,8 +8,8 @@ struct cpu {
   unsigned char pc;
 
   // registers (array)
-  unsigned char registers[8];  // 8 registers
-  
+  unsigned char reg[8];  // 8 registers
+
   // ram (array)
   unsigned char ram[256]; // 256 bytes
 
@@ -31,11 +31,12 @@ enum alu_op {
 #define LDI  0b10000010
 #define PRN  0b01000111
 #define HLT  0b00000001
+#define MUL  0b10100010 
 // TODO: more instructions here. These can be used in cpu_run().
 
 // Function declarations
 
-extern void cpu_load(struct cpu *cpu);
+extern void cpu_load(struct cpu *cpu, char *filename);
 extern void cpu_init(struct cpu *cpu);
 extern void cpu_run(struct cpu *cpu);
 
