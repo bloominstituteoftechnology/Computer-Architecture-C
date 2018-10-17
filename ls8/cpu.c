26f64a0192cb16edbb42610867320f49c37c09c4
M.Real <mreal126@protonmail.com>
2018-10-17 17:44:03 -0400

    still working on step7

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,6 +1,7 @@
 #include "cpu.h"
 #include <string.h>
 #include <stdio.h>
+#include <stlib.h>
 
 #define DATA_LEN 6
 
@@ -14,25 +15,25 @@ void cpu_ram_write(struct cpu *cpu, unsigned char mar, unsigned char mdr)
   cpu->ram[mar] = mdr;
 }
 
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char *filename)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
-
-  int address = 0;
-
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  FILE *fp = fopen(filename, "r");
+  char line(1024);
+  unsigned char addr = 0x00;
+    
+  if (fp == NULL) {
+    fprintf(stderr, "error opening file %s\n", filename);
+    exit(2);
   }
 
-  // TODO: Replace this with something less hard-coded
+  while (fgets(line, sizeof line, fp) !=NULL) {
+    char *endptr;
+
+    unsigned char b = strtoul(line, &endptr, 2);
+
+    cpu_ram_write(cpu, addr++, b);
+  }
+  fclose(fp);
 }
 
 /**
@@ -40,6 +41,10 @@ void cpu_load(struct cpu *cpu)
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
+  (void)cpu;
+  (void)regA;
+  (void)regB;
+
   switch (op) {
     case ALU_MUL:
       // TODO
