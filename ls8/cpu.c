cc65777b10230390a4b663c7c56a5d6cd3b75096
Jasper <joshtietz11@gmail.com>
2018-10-17 18:20:57 -0500

    Joshua Tietz -- Comp Architecture
    still having isuues geting the desired returns in the terminal. got some more to work on for day 3 mvp.

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,13 +1,28 @@
 #include "cpu.h"
+#include <stdio.h>
+#include <string.h>
 
 #define DATA_LEN 6
+
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char index)
+{
+  return cpu->ram[index];
+}
+
+// Write to memory
+void cpu_ram_write(struct cpu *cpu, unsigned char index, unsigned char val)
+{
+  cpu->ram[index] = val;
+}
+
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
 void cpu_load(struct cpu *cpu)
 {
-  char data[DATA_LEN] = {
+  printf("\nCPU Loading....\n");
+  unsigned char data[DATA_LEN] = {
     // From print8.ls8
     0b10000010, // LDI R0,8
     0b00000000,
@@ -19,7 +34,9 @@ void cpu_load(struct cpu *cpu)
 
   int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
+  int i;
+  for (i = 0; i < DATA_LEN; i++) {
+    // printf("value in address: %d", cpu->ram[address]);
     cpu->ram[address++] = data[i];
   }
 
@@ -35,7 +52,6 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
     case ALU_MUL:
       // TODO
       break;
-
     // TODO: implement more ALU ops
   }
 }
@@ -47,13 +63,55 @@ void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
+  unsigned char IR; //instruction register
+ 
+  printf("\nCPU Running....\n");
   while (running) {
-    // TODO
     // 1. Get the value of the current instruction (in address PC).
-    // 2. switch() over it to decide on a course of action.
-    // 3. Do whatever the instruction should do according to the spec.
+
+    IR = cpu_ram_read(cpu, cpu->PC);
+    unsigned char operandA = cpu_ram_read(cpu, (cpu->PC+1) & 0xFF);
+    unsigned char operandB = cpu_ram_read(cpu, (cpu->PC+2) & 0xFF);
+
+
+    printf("\nTrace:\n");
+    printf("PC: %02X\n" , cpu->PC);
+    printf("Instruct Reg: %02X\n", IR); 
+    printf("Operand A: %02X\n", operandA);
+    printf("Operand B: %02X\n", operandB);
+
+
+    int add_to_pc = (IR >> 6) + 1;
+      // 2. switch() over it to decide on a course of action.
+    switch (IR) {
+      case LDI:
+        // LDI
+        // 3. Do whatever the instruction should do according to the spec.
+        printf("Command is LDI...\n");
+        cpu->reg[operandA] = operandB;
+        break;
+      
+      case PRN:
+        printf("%d\n", cpu->reg[operandA]);
+        break;
+
+      case MUL:
+        printf("Command is MUL....\n");
+        cpu->reg[operandA] = cpu->reg[operandA] * cpu->reg[operandB];
+        
+        break;
+
+      case HLT:
+        printf("Command is HLT! \n");
+        running = 0;
+        break;
+    }
+
     // 4. Move the PC to the next instruction.
+    cpu->PC += add_to_pc;
   }
+
+
 }
 
 /**
@@ -61,7 +119,21 @@ void cpu_run(struct cpu *cpu)
  */
 void cpu_init(struct cpu *cpu)
 {
-  // TODO: Initialize the PC and other special registers
+  // Power On State
+  // R0-R6 are cleared to 0.
+  // R7 is set to 0xF4.
+ 
+  // PC and FL registers are cleared to 0.
+  // RAM is cleared to 0.
+  printf("CPU Init starting...\n");
+  
+  // Initialize the PC and other special registers
+  cpu->PC = 0;
 
-  // TODO: Zero registers and RAM
+  // // TODO: Zero registers and RAM
+  memset(cpu->ram, 0, sizeof(cpu->ram));
+  memset(cpu->reg, 0, sizeof(cpu->reg));
+
+ cpu->reg[7] = 0b11110100;
 }
+
--- ls8/cpu.h
+++ ls8/cpu.h
@@ -3,16 +3,16 @@
 
 // Holds all information about the CPU
 struct cpu {
-  // TODO
-  // PC
-  // registers (array)
-  // ram (array)
+  unsigned char PC;
+  unsigned char reg[8];
+  unsigned char ram[256];
+
 };
 
 // ALU operations
 enum alu_op {
-	ALU_MUL
-	// Add more here
+	ALU_MUL,
+  ALU_ADD
 };
 
 // Instructions
@@ -20,13 +20,22 @@ enum alu_op {
 // These use binary literals. If these aren't available with your compiler, hex
 // literals should be used.
 
-#define LDI  0b10000010
+#define PRN 0b01000111
+#define HLT 0b00000001
+#define LDI 0b10000010
+#define MUL 0b10100010
+
+
+
 // TODO: more instructions here. These can be used in cpu_run().
 
 // Function declarations
 
 extern void cpu_load(struct cpu *cpu);
+extern void cpu_load_from_file(struct cpu *cpu, unsigned char filepath);
+
 extern void cpu_init(struct cpu *cpu);
 extern void cpu_run(struct cpu *cpu);
-
+extern void cpu_ram_write(struct cpu *cpu, unsigned char mar, unsigned char mdr);
+// extern void cpu_ram_read(struct cpu *cpu, unsigned char mar);
 #endif
