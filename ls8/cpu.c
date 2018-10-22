b7a1bc9a6adc9fe13dec1444a9b5076a6d3be74b
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-10-22 15:53:24 -0700

    initial commit

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -7,23 +7,24 @@
  */
 void cpu_load(struct cpu *cpu)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+    char data[DATA_LEN] = {
+        // From print8.ls8
+        0b10000010, // LDI R0,8
+        0b00000000,
+        0b00001000,
+        0b01000111, // PRN R0
+        0b00000000,
+        0b00000001 // HLT
+    };
 
-  int address = 0;
+    int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
-  }
+    for (int i = 0; i < DATA_LEN; i++)
+    {
+        cpu->ram[address++] = data[i];
+    }
 
-  // TODO: Replace this with something less hard-coded
+    // TODO: Replace this with something less hard-coded
 }
 
 /**
@@ -31,13 +32,14 @@ void cpu_load(struct cpu *cpu)
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
-  switch (op) {
+    switch (op)
+    {
     case ALU_MUL:
-      // TODO
-      break;
+        // TODO
+        break;
 
-    // TODO: implement more ALU ops
-  }
+        // TODO: implement more ALU ops
+    }
 }
 
 /**
@@ -45,15 +47,16 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
  */
 void cpu_run(struct cpu *cpu)
 {
-  int running = 1; // True until we get a HLT instruction
+    int running = 1; // True until we get a HLT instruction
 
-  while (running) {
-    // TODO
-    // 1. Get the value of the current instruction (in address PC).
-    // 2. switch() over it to decide on a course of action.
-    // 3. Do whatever the instruction should do according to the spec.
-    // 4. Move the PC to the next instruction.
-  }
+    while (running)
+    {
+        // TODO
+        // 1. Get the value of the current instruction (in address PC).
+        // 2. switch() over it to decide on a course of action.
+        // 3. Do whatever the instruction should do according to the spec.
+        // 4. Move the PC to the next instruction.
+    }
 }
 
 /**
@@ -61,7 +64,7 @@ void cpu_run(struct cpu *cpu)
  */
 void cpu_init(struct cpu *cpu)
 {
-  // TODO: Initialize the PC and other special registers
+    // TODO: Initialize the PC and other special registers
 
-  // TODO: Zero registers and RAM
+    // TODO: Zero registers and RAM
 }
