48c572f93f22c25ee8687c95c2a473bc0793c86e
ari7946 <mercado7946@gmail.com>
2018-10-24 09:41:08 -0700

    reads current instruction to decide course of action

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -57,11 +57,32 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
+  unsigned char IR, operandA, operandB; 
 
   while (running) {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
+    IR = cpu_ram_read(cpu, cpu->PC);
+    operandA = cpu_ram_read(cpu, cpu->PC+1);
+    operandB = cpu_ram_read(cpu, cpu->PC+2);
+
     // 2. switch() over it to decide on a course of action.
+    switch(IR) {
+      case LDI:
+        cpu->reg[operandA] = operandB;
+        cpu->PC += 3;
+        break;
+
+      case PRN:
+        print("%d\n", cpu->reg[operandA]);
+        cpu->PC += 2;
+        break;
+
+      case HLT:
+        running = 0;
+        cpu->PC += 1;
+        break;
+    }
     // 3. Do whatever the instruction should do according to the spec.
     // 4. Move the PC to the next instruction.
   }
