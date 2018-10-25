08def4b1a36dfb7a620e0a5ae9d37238e3abb84e
Jesse Reichel <Jesse.Reichel@live.com>
2018-10-24 22:06:48 -0700

    working print8.ls8

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -56,9 +56,10 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 void cpu_run(struct cpu *cpu)
 {
   unsigned char IR, operandA, operandB, operandC, operandD;
+  int instruction_index;
+
 
 // gives LDI
-  IR = cpu_ram_read(cpu, cpu->pc);
 
   int running = 1; // True until we get a HLT instruction
 
@@ -66,30 +67,44 @@ void cpu_run(struct cpu *cpu)
 //  operandB = cpu_ram_read(cpu, cpu->pc + 2); // 8 value
 
 
- printf("A: %u\n B: %u\n", operandA, operandB);
+//  printf("A: %u\n B: %u\n", operandA, operandB);
+  // printf("%d \n", instruction_index);
+  // printf("%u \n", IR);
+
+  // IR = cpu_ram_read(cpu, instruction_index);
+
+  instruction_index = cpu->pc;
+
 
   while (running) {
+  IR = cpu_ram_read(cpu, instruction_index);
+
+
     // TODO
     // 1. Get the value of the current instruction (in address PC).
 
      // r0
-     operandA = cpu_ram_read(cpu, cpu->pc + 1); 
 
     // 8 value
-     operandB = cpu_ram_read(cpu, cpu->pc + 2);
+
 
 
     // 2. switch() over it to decide on a course of action.
      switch(IR) {
 
-       case LDI:
+      case LDI:
+      operandA = cpu_ram_read(cpu, instruction_index + 1); 
+      operandB = cpu_ram_read(cpu, instruction_index + 2);
+
         cpu->registers[operandA] = operandB;
-        IR += 3;
+        instruction_index += 3;
         break;
 
        case PRN:
+        operandA = cpu_ram_read(cpu, instruction_index + 1); 
+
         printf("%d\n", cpu->registers[operandA]);
-        IR += 2;
+        instruction_index += 2;
         break;
 
       case HLT:
@@ -98,7 +113,7 @@ void cpu_run(struct cpu *cpu)
 
 
        default:
-        printf("Nothing to run");
+        printf("Nothing to run \n");
         running = 0;
      } 
     // 3. Do whatever the instruction should do according to the spec.
