d512824a2c5059f8687766a6c0587fe4f18b494d
unknown <alexisreyes90@gmail.com>
2018-10-24 23:37:13 -0400

    added binary code for HLT and PRN for case scenario

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -21,6 +21,8 @@ enum alu_op {
 // literals should be used.
 
 #define LDI  0b10000010
+#define PRN  0b01000111
+#define HLT  0b00000001 
 // TODO: more instructions here. These can be used in cpu_run().
 
 // Function declarations
