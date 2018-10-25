8696e956889f8b852b06b44104a7958b96acccb3
Mark Hong <m4rkh0ng@gmail.com>
2018-10-25 18:05:44 -0400

    day 4 learning

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -4,25 +4,23 @@
 // Holds all information about the CPU
 struct cpu {
   // TODO
-  // PC
-  unsigned char PC;
+  // PC Program counter. Index of which instruction we need to execute
+  int PC;
   // registers (array)
-  unsigned char reg[8];
+  unsigned char registers[8];
   // ram (array)
   unsigned char ram[256];
+  // Flags
+  unsigned char FL;
 };
 
 // ALU operations
 enum alu_op {
 	ALU_MUL,
-  ALU_ADD,
-  LOAD_VALUE,
-  NOTHING
 	// Add more here
+  NOTHING
 };
 
-#define ADDR_PROGRAM_ENTRY 0x00 // Load
-#define ADD_EMPTY_STACK 0xF4 // Empty Stack
 
 // Instructions
 
@@ -33,12 +31,44 @@ enum alu_op {
 #define PRN  0b01000111
 #define HLT  0b00000001
 #define MUL  0b10100010
+#define PUSH  0b01000101
+#define POP 0b01000110
+#define ADD 0b10100000
+#define CALL 0b01010000 
+#define RET 0b00010001
+#define ST 0b10000100
+#define CMP 0b10100111
+#define JMP 0b01010100
+#define JEQ 0b01010101
+#define JNE 0b01010110
+#define PRA 0b01001000
+#define IRET 0b00010011
+
 // TODO: more instructions here. These can be used in cpu_run().
 
 // Function declarations
 
-extern void cpu_load(char *filename, struct cpu *cpu);
+extern void cpu_load(struct cpu *cpu, char* fileName);
 extern void cpu_init(struct cpu *cpu);
 extern void cpu_run(struct cpu *cpu);
+extern unsigned char cpu_ram_read(struct cpu *cpu, int mar);
+extern void cpu_ram_write(struct cpu *cpu, int mar, unsigned char value);
+extern int handle_LDI(struct cpu* cpu, unsigned char regA, unsigned char regB);
+extern int handle_PRN(struct cpu* cpu, unsigned char regA, unsigned char regB);
+extern int handle_HLT(struct cpu* cpu, unsigned char regA, unsigned char regB);
+extern int handle_MUL(struct cpu* cpu, unsigned char regA, unsigned char regB);
+extern int handle_PUSH(struct cpu* cpu, unsigned char regA, unsigned char regB);
+extern int handle_POP(struct cpu* cpu, unsigned char regA, unsigned char regB);
+extern int handle_ADD(struct cpu* cpu, unsigned char regA, unsigned char regB);
+extern int handle_CALL(struct cpu* cpu, unsigned char regA, unsigned char regB);
+extern int handle_RET(struct cpu* cpu, unsigned char regA, unsigned char regB);
+extern int handle_ST(struct cpu* cpu, unsigned char regA, unsigned char regB);
+extern int handle_CMP(struct cpu* cpu, unsigned char regA, unsigned char regB);
+extern int handle_JMP(struct cpu* cpu, unsigned char regA, unsigned char regB);
+extern int handle_JEQ(struct cpu* cpu, unsigned char regA, unsigned char regB);
+extern int handle_JNE(struct cpu* cpu, unsigned char regA, unsigned char regB);
+extern void handle_interrupt(struct cpu* cpu);
+extern int handle_IRET(struct cpu* cpu, unsigned char regA, unsigned char regB);
+
 
 #endif
