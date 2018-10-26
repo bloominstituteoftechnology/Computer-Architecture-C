ad0a6fc71dd60776387a07c864e51de7f31907c0
Daniel Chamorro <dan@dcitllc.com>
2018-10-26 13:11:06 -0400

    ls8.c

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -4,12 +4,18 @@
 /**
  * Main
  */
-int main(void)
+int main(int argc, char **argv)
 {
   struct cpu cpu;
 
+  if (argc != 2)
+  {
+    fprintf(stderr, "usage: ls8 file.ls8\n");
+    return 1;
+  }
+
   cpu_init(&cpu);
-  cpu_load(&cpu);
+  cpu_load(argv[1], &cpu);
   cpu_run(&cpu);
 
   return 0;
