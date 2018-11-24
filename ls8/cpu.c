f802a4b80d421a619aebf466ee76ea95ecd2cd87
katiagilligan888 <katia.gilligan88@gmail.com>
2018-11-24 11:41:55 -0700

    Added cpu init function which initializes all values of registers, PC, and ram

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -69,7 +69,9 @@ void cpu_run(struct cpu *cpu)
  */
 void cpu_init(struct cpu *cpu)
 {
-  // TODO: Initialize the PC and other special registers
+  cpu-> PC = 0; 
 
-  // TODO: Zero registers and RAM
+  //  function memset copies the character c (an unsigned char) to the first n characters of the string pointed to, by the argument str.
+  memset(cpu-> ram, 0, sizeof cpu-> ram); 
+  memset(cpu-> registers, 0, sizeof cpu-> registers); 
 }
