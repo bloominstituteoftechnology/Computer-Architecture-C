c7e0d2f0a48ddbd415d5317580c040ca62b36dfd
katiagilligan888 <katia.gilligan88@gmail.com>
2018-11-24 12:46:10 -0700

    Added some cpu_run functionality, still needs work

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -56,10 +56,23 @@ void cpu_run(struct cpu *cpu)
   int running = 1; // True until we get a HLT instruction
 
   while (running) {
-    // TODO
-    // 1. Get the value of the current instruction (in address PC).
-    // 2. switch() over it to decide on a course of action.
-    // 3. Do whatever the instruction should do according to the spec.
+    
+      unsigned char IR = cpu_ram_read(cpu, cpu-> PC); 
+      unsigned char operandA = cpu_ram_read(cpu, cpu-> PC + 1); 
+      unsigned char operandB = cpu_ram_read(cpu, cpu-> PC + 2); 
+
+      switch(IR){
+        case LDI: // opcode for setting value of register to integer
+          cpu-> registers[operandA] = operandB; //operands R0 = operandA and 8 = operandB
+          break; 
+        case PRN: // opcode for printing numeric value at the register
+          printf('%d \n', registers[operandA]); 
+          break; 
+        case HLT: // opcode to exit the emulator
+          running = 0; 
+          break; 
+      }
+    
     // 4. Move the PC to the next instruction.
   }
 }
