bcb91b787324a1a08f06f78bb588ce64be8da3fe
francistse23 <wtse.ucsd@gmail.com>
2018-11-26 12:00:23 -0800

    Finished cpu struct

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -4,9 +4,9 @@
 // Holds all information about the CPU
 struct cpu {
   // TODO
-  // PC
-  // registers (array)
-  // ram (array)
+  unsigned char PC;
+  unsigned char reg[8];
+  unsigned char ram[256];
 };
 
 // ALU operations
