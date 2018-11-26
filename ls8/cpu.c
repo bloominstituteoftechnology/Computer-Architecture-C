49db6d06a7f2a17e58cca81d0e9dbf9a76bc7ac8
Grant Reighard <grant.reighard@gmail.com>
2018-11-26 17:35:11 -0500

    Implemented cpu_init().

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -62,8 +62,12 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
-
   // TODO: Zero registers and RAM
+  memset(cpu->registers, 0, 7);
+  cpu->registers[7] = 0xF4;
+  cpu->PC = 0;
+  cpu->FL = 0;
+  memset(cpu->ram, 0, 256);
 }
 
 unsigned char cpu_ram_read(struct cpu *cpu, unsigned char index)
