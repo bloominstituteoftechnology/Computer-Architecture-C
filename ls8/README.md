11c8b0d39aefa5bed9299d3bb3aa6febb83e778c
mkerbleski <mkerbles@gmail.com>
2018-11-26 14:31:22 -0700

    added ALU commands

--- ls8/README.md
+++ ls8/README.md
@@ -62,6 +62,9 @@ but you'll have to implement those three above instructions first!
 
 * Make a list of files here.
 * Write a short 3-10-word description of what each file does.
+    cpu - mocks the function of the cpu
+    ls8.c - initializes cpu
+    
 * Note what has been implemented, and what hasn't.
 * Read this whole file.
 * Skim the spec.
--- ls8/cpu.h
+++ ls8/cpu.h
@@ -5,13 +5,48 @@
 struct cpu {
   // TODO
   // PC
+  unsigned char x;
   // registers (array)
+  char *registers;
   // ram (array)
+  char *ram;
 };
 
 // ALU operations
 enum alu_op {
-	ALU_MUL
+	ALU_MUL,
+  ALU_ADD,
+  ALU_AND,
+  ALU_CALL,
+  ALU_CMP,
+  ALU_DEC,
+  ALU_DIV,
+  ALU_HLT,
+  ALU_INC,
+  ALU_IRET,
+  ALU_JEQ,
+  ALU_JGE,
+  ALU_JGT,
+  ALU_JLE,
+  ALU_JLT,
+  ALU_JMP,
+  ALU_JNE,
+  ALU_LD,
+  ALU_LDI,
+  ALU_MOD,
+  ALU_NOP,
+  ALU_NOT,
+  ALU_OR,
+  ALU_POP,
+  ALU_PRA,
+  ALU_PRN,
+  ALU_PUSH,
+  ALU_RET,
+  ALU_SHR,
+  ALU_ST,
+  ALU_SUB,
+  ALU_XOR
+
 	// Add more here
 };
 
