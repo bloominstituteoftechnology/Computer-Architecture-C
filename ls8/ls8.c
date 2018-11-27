33906c53cdc7e79b6ef5d2990989c47109236226
codejoncode <jonathanjholloway@gmail.com>
2018-11-27 16:31:19 -0600

    getting unused variable argc warning/error implemented a check to see if argc is truthy if it isn't a error prints out letting the user know that arguments are needed to use the cpu_load function

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -6,11 +6,17 @@
  */
 int main(int argc, char *argv[])
 {
+  if(!argc){
+    printf("No arguments entered, while cpu_load requires an argument\n");
+  }
+  
   struct cpu cpu;
 
   cpu_init(&cpu);
   cpu_load(&cpu, argv[1]);
   cpu_run(&cpu);
+
+  
 
   return 0;
 }
