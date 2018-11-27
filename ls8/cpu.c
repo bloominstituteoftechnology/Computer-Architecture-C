99b1fb52588b14e6847328ebdcd3a37dc56c861a
skennedy93 <sean47900@gmail.com>
2018-11-27 16:29:54 -0600

    working on getting the cpu load un hardcoded

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,6 +1,7 @@
 #include "cpu.h"
 #include <stdio.h>
 #include <string.h>
+#include "stdlib.h"
 
 #define DATA_LEN 6
 
@@ -15,25 +16,35 @@ void cpu_ram_write(struct cpu *cpu, int storage, unsigned char value) {
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array.
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char *file)
 {
-  char data[DATA_LEN] = {
+  //char data[DATA_LEN] = {
     // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+    //0b10000010, // LDI R0,8
+    //0b00000000,
+    //0b00001000,
+    //0b01000111, // PRN R0
+    //0b00000000,
+    //0b00000001  // HLT
+  //};
 
-  int address = 0;
+  //int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
-  }
+  //for (int i = 0; i < DATA_LEN; i++) {
+    //cpu->ram[address++] = data[i];
+  //}
 
   // TODO: Replace this with something less hard-coded
+  FILE *fp;
+  char line[500];
+  unsigned char address = 0; 
+  fp = fopen(file, "r"); 
+
+    if (fp == NULL){
+    printf("Cannot Open File.\n", fp);
+    exit(2);
+    }
+
 }
 
 /**
