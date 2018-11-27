d05fc6b7c4e08f0e91ae145e61a31aacd91756b4
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-11-27 16:26:24 -0500

    just need to do the beautify step

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,29 +1,35 @@
 #include "cpu.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define DATA_LEN 6
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char *filename)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
-
+  // char data[DATA_LEN] = {
+  //   // From print8.ls8
+  //   0b10000010, // LDI R0,8
+  //   0b00000000,
+  //   0b00001000,
+  //   0b01000111, // PRN R0
+  //   0b00000000,
+  //   0b00000001  // HLT
+  // };
+  FILE *fp = fopen(filename, "r");
   int address = 0;
-
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  char c[8192];
+  while (fgets(c, sizeof c, fp) != NULL) {
+   cpu->ram[address++] = strtoul(c, NULL, 2);
   }
+  fclose(fp);
+
+ 
+
+ 
 
   // TODO: Replace this with something less hard-coded
 }
@@ -80,6 +86,10 @@ void cpu_run(struct cpu *cpu)
       case PRN:
         printf("PRN\n");
         printf("%d\n", cpu->registers[(int)operandA]);
+        break;
+      case MUL:
+        printf("MUL\n");
+        cpu->registers[(int)operandA] =  cpu->registers[(int)operandA] *  cpu->registers[(int)operandB];
         break;
       default:
         printf("%d", IR);
