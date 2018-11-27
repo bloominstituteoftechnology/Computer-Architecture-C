d6d437dfdcc41ff083a6a50594c1ab8ac7768fae
fron12 <sohnf90@yahoo.com>
2018-11-27 15:57:37 -0800

    Day 2 Progress: changed main signature and cpu_load in ls8.c

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -17,21 +17,21 @@ void cpu_ram_write(struct cpu *cpu, int index, unsigned char value) {
  */
 void cpu_load(struct cpu *cpu)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+  // char data[DATA_LEN] = {
+  //   // From print8.ls8
+  //   0b10000010, // LDI R0,8
+  //   0b00000000,
+  //   0b00001000,
+  //   0b01000111, // PRN R0
+  //   0b00000000,
+  //   0b00000001  // HLT
+  // };
 
-  int address = 0;
+  // int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
-  }
+  // for (int i = 0; i < DATA_LEN; i++) {
+  //   cpu->ram[address++] = data[i];
+  // }
 
   // TODO: Replace this with something less hard-coded
 }
@@ -80,6 +80,7 @@ void cpu_run(struct cpu *cpu)
 
       case HLT:
         printf("Halting...\n");
+        cpu->PC += 1;
         running = 0;
         break;
     }
