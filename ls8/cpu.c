45b5dcd858eb967b1ebdd30e53fcb823ac93ef64
Keegan Burkett <keegan.t.burkett@gmail.com>
2018-11-27 15:59:17 -0600

    Added cpu run switch

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,4 +1,5 @@
 #include "cpu.h"
+#include <stdio.h>
 
 #define DATA_LEN 6
 
@@ -64,9 +65,30 @@ void cpu_run(struct cpu *cpu)
   while (running) {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
+    unsigned char CI = cpu_ram_read(cpu, cpu->PC);
+
+    unsigned char operand_a = cpu_ram_read(cpu, (cpu->PC + 1) & 0xff);
+    unsigned char operand_b = cpu_ram_read(cpu, (cpu->PC + 2) & 0xff);
     // 2. switch() over it to decide on a course of action.
     // 3. Do whatever the instruction should do according to the spec.
     // 4. Move the PC to the next instruction.
+    switch(CI) {
+      case LDI:
+      printf("\nLDI: R%d: stored value is: %d\n\n", operand_a, operand_b);
+      cpu->reg[operand_a] = operand_b;
+      break;
+    case PRN:
+      printf("\nPRN: R%d: value is %d\n\n", operand_a, cpu->reg[operand_a]);
+      break;
+    case HLT:
+      printf("\nHLT: Program halted\n\n");
+      running = 0;
+      exit(0);
+    default:
+      printf("\nThe instruction doesn't exist\n\n");
+      exit(1);
+    }
+    
   }
 }
 
@@ -79,7 +101,7 @@ void cpu_init(struct cpu *cpu)
   cpu->PC = 0;
   // TODO: Zero registers and RAM
   memset(cpu->ram, 0, sizeof(cpu->ram));
-  memset(cpu->registers, 0, sizeof(cpu->registers));
+  memset(cpu->reg, 0, sizeof(cpu->reg));
 
   //Later on, you might do further initialization here, e.g. setting the initial value of the stack pointer.
 }
--- ls8/cpu.h
+++ ls8/cpu.h
@@ -7,7 +7,7 @@ struct cpu {
   // PC
   unsigned char PC;
   // registers (array)
-  unsigned char registers[8];
+  unsigned char reg[8];
   // ram (array)
   unsigned char ram[256];
 };
@@ -25,7 +25,8 @@ enum alu_op {
 
 #define LDI  0b10000010
 // TODO: more instructions here. These can be used in cpu_run().
-
+#define PRN 0b01000111
+#define HLT 0b00000001
 // Function declarations
 
 extern void cpu_load(struct cpu *cpu);
