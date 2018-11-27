0e9c79221bd92782f246a6f3b4cef175ff34a570
William <kwontaewoongah@gmail.com>
2018-11-27 15:26:34 -0800

    use of argc and argv for main function

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -4,12 +4,16 @@
 /**
  * Main
  */
-int main(void)
+int main(int argc, char *argv[])
 {
   struct cpu cpu;
 
   cpu_init(&cpu);
-  cpu_load(&cpu,"./examples/mult.ls8");
+  if (argc==2) {
+    cpu_load(&cpu,argv[1]);
+  } else {
+    cpu_load(&cpu,"./examples/print8.ls8");
+  }
   cpu_run(&cpu);
 
   return 0;
