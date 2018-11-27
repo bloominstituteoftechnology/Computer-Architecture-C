130a5e1dc11270825d1fad186cf0ee99c2c17302
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-11-27 16:19:26 -0500

    loading program from file instead of hardcoded print8

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -18,25 +18,40 @@ void cpu_ram_write(struct cpu *cpu, unsigned char pos, unsigned char value)
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char *path)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
-
-  int address = 0;
-
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
-  }
+  // char data[DATA_LEN] = {
+  //   // From print8.ls8
+  //   0b10000010, // LDI R0,8
+  //   0b00000000,
+  //   0b00001000,
+  //   0b01000111, // PRN R0
+  //   0b00000000,
+  //   0b00000001  // HLT
+  // };
+  //
+  // int address = 0;
+  //
+  // for (int i = 0; i < DATA_LEN; i++) {
+  //   cpu->ram[address++] = data[i];
+  // }
 
   // TODO: Replace this with something less hard-coded
+  FILE *fp;
+  char line[8192];
+  int address = 0;
+
+  if((fp = fopen(path, "r")) == NULL){
+    printf("Error opening file.\n");
+    exit(2);
+  }
+
+  while(fgets(line, sizeof line, fp) != NULL) {
+    char *endptr;
+    unsigned char curr = strtol(line, &endptr, 2);
+
+    cpu->ram[address++] = curr;
+  }
 }
 
 /**
--- ls8/ls8.c
+++ ls8/ls8.c
@@ -4,13 +4,17 @@
 /**
  * Main
  */
-int main(void)
+int main(int argc, char *argv[])
 {
   struct cpu cpu;
 
-  cpu_init(&cpu);
-  cpu_load(&cpu);
-  cpu_run(&cpu);
-
+  //add in argv check here
+  if(argc != 2) {
+    printf("Correct usage: ./ls8 <path>\n");
+  }else{
+    cpu_init(&cpu);
+    cpu_load(&cpu, argv[1]);
+    cpu_run(&cpu);
+  }
   return 0;
 }
