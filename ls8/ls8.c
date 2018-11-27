21b6654c944d6eee82a0063087814daf4e118651
Erik Kimsey <erik@erikkimsey.com>
2018-11-27 17:54:24 -0500

    Added FILE and arg read functionality.  Checks for proper argument input from cmd line.  Am sending a reference to open file from main to cpu_load.  In cpu_load am applying file data to RAM...or thats what Im coding at the moment.

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -6,13 +6,22 @@
  */
 int main(int argc, char *argv[])
 {
+
+  FILE *fp;
   struct cpu cpu;
 
-  cpu_init(&cpu);
-  cpu_load(&cpu);
-  cpu_run(&cpu);
-
-  
+  if(argc < 2){
+    printf("You need run the ./ls8 with another argument");
+    exit(1);
+  } else if(argv[0] == NULL || argv[1] == NULL){
+    printf("You need run the ./ls8 with another argument");
+    exit(1);
+  } else {
+    fp = fopen(argv[1], "r");
+    cpu_init(&cpu);
+    cpu_load(&cpu, &fp);
+    cpu_run(&cpu);
+  }
 
   return 0;
 }
