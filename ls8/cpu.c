2ca4dd86ce2ac590f368b9b7996bf085dd8f3001
James Phillips <bri79319@gmail.com>
2018-11-27 10:58:42 -0500

    got Day 1 done with Nando's helped in before hours

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,9 +1,19 @@
 #include "cpu.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define DATA_LEN 6
 
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char index)
+{
+  return cpu->ram[index];
+}
+
+void cpu_ram_write(struct cpu *cpu, unsigned char index, unsigned char value)
+{
+  cpu->ram[index] = value;
+}
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
@@ -54,21 +64,39 @@ void cpu_run(struct cpu *cpu)
   while (running)
   {
     // TODO
-    // 1. Get the value of the current instruction (in address PC).
+    // 1. Get the value of the current instruction (at the address PC).
+    unsigned char curr_instruct = cpu_ram_read(cpu, cpu->PC);
     // 2. switch() over it to decide on a course of action.
+
+    unsigned char value1 = cpu_ram_read(cpu, cpu->PC + 1);
+    unsigned char value2 = cpu_ram_read(cpu, cpu->PC + 2);
+
+    switch (curr_instruct)
+    {
+
+    case LDI:
+
+      cpu->reg[value1] = value2;
+
+      cpu->PC += 3;
+
+      break;
+
+    case PRN:
+      printf("%u\n", cpu->reg[value1]);
+      cpu->PC += 2;
+      break;
+
+    case HLT:
+      exit(0);
+      break;
+
+    default:
+      exit(1);
+    }
     // 3. Do whatever the instruction should do according to the spec.
     // 4. Move the PC to the next instruction.
   }
-}
-
-unsigned char cpu_ram_read(struct cpu *cpu, int index)
-{
-  return cpu->ram[index];
-}
-
-void cpu_ram_write(struct cpu *cpu, int index, unsigned char value)
-{
-  cpu->ram[index] = value;
 }
 
 /**
@@ -77,9 +105,9 @@ void cpu_ram_write(struct cpu *cpu, int index, unsigned char value)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
-  int PC = memset(0);
+  cpu->PC = 0;
+  memset(cpu->reg, 0, sizeof(cpu->reg));
+  memset(cpu->ram, 0, sizeof(cpu->ram));
 
   // TODO: Zero registers and RAM
-  unsigned char registers[8] = 0;
-  int ram[256] = 0;
 }
--- ls8/cpu.h
+++ ls8/cpu.h
@@ -30,6 +30,8 @@ enum alu_op
 
 #define LDI 0b10000010
 // TODO: more instructions here. These can be used in cpu_run().
+#define PRN 0b01000111
+#define HLT 0b00000001
 
 // Function declarations
 
