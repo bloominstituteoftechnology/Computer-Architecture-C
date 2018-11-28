36b999b5a0624e46928a8315dd54f501e872d2c0
LizBaker <lizbaker.pdx@gmail.com>
2018-11-28 13:36:59 -0800

    finished out multiply and stack -will work on beautifying my terrible cpu_run

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -14,11 +14,13 @@ void cpu_load(struct cpu *cpu, int argc, char *arg)
   } else {
     FILE *fp;
     fp = fopen(arg, "r");
+    if (fp == NULL){
+      printf("could not locate file");
+    }
     char instruction[9];
     for (int i=0; i<256; i++){
       if (fgets(instruction, 9, fp) != NULL) {
         cpu->ram[i] = strtoul(instruction, NULL, 2);
-        printf("wats in tha ram: %d\n", cpu->ram[i]);
         while(fgetc(fp) != '\n');
       } else {
         break;
@@ -26,27 +28,20 @@ void cpu_load(struct cpu *cpu, int argc, char *arg)
     }
     fclose(fp);
   }
-
-  // int address = 0;
-
-  // for (int i = 0; i < DATA_LEN; i++) {
-  //   cpu->ram[address++] = data[i];
-  // }
-  // TODO: Replace this with something less hard-coded
 }
 
 /**
  * ALU
  */
-void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
+int alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
-  // switch (op) {
-  //   case ALU_MUL:
-  //     // TODO
-  //     break;
+  switch (op) {
+    case ALU_MUL:
+      return regA * regB;
+      break;
 
-  //   // TODO: implement more ALU ops
-  // }
+    // TODO: implement more ALU ops
+  }
 }
 
 char cpu_ram_read(struct cpu *cpu, int index)
@@ -69,10 +64,31 @@ void cpu_run(struct cpu *cpu)
   while (running) {
     int c = cpu->PC;
     unsigned char instruction = cpu->ram[c];
+    unsigned char regA = cpu->registers[(cpu->ram[c+1])];
+    unsigned char regB = cpu->registers[(cpu->ram[c+2])];
+    unsigned char SP = cpu->registers[7];
+    unsigned char SP_Val = cpu->ram[SP];
     switch(instruction) {
       case LDI:
         cpu->registers[(cpu->ram[c+1])] = cpu->ram[c+2];
-        printf("ram write: %d\n", cpu->registers[0]);
+        printf("register: %d\n", cpu->registers[(cpu->ram[c+1])]);
+        cpu->PC += 3;
+        break;
+      case PUSH:
+      printf("push\n");
+        --cpu->registers[7];
+        cpu->ram[cpu->registers[7]] = cpu->registers[(cpu->ram[c+1])];
+        cpu->PC += 2;
+        break;
+      case POP:
+      printf("pop\n");
+        cpu->registers[(cpu->ram[c+1])] = cpu->ram[cpu->registers[7]];
+        ++cpu->registers[7];
+        cpu->PC += 2;
+        break;
+      case ALU_MUL:
+        printf("mul: %d * %d\n", regA, regB);
+        cpu->registers[(cpu->ram[c+1])] = alu(cpu, ALU_MUL, regA, regB);
         cpu->PC += 3;
         break;
       case PRN:
@@ -98,6 +114,7 @@ void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
   cpu->PC = 0;
+  cpu->registers[7] = 0xF3;
   // TODO: Zero registers and RAM
   memset(cpu->registers[0], 8, 0);
   memset(cpu->ram[0], 256, 0);
