9a1d0d8231ac4f30397e0af0450b7ccb45e71718
fron12 <sohnf90@yahoo.com>
2018-11-28 15:49:58 -0800

    Day 2 Done: mult8

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "cpu.h"
+#include <stdlib.h>
 
 #define DATA_LEN 6
 
@@ -15,8 +16,29 @@ void cpu_ram_write(struct cpu *cpu, int index, unsigned char value) {
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char *argv[])
 {
+  FILE *fp = fopen(argv[1], "r");
+  char line [1024];
+  unsigned char address = 0;
+
+  if(fp == NULL) {
+    fprintf(stderr, "Couldn't open the file %s\n", argv[1]);
+    exit(2);
+  }
+
+  while(fgets(line, sizeof line, fp) != NULL){
+    unsigned char bc = strtoul(line, NULL, 2);
+
+    if(line[0] == '\n' || line[0] == '#'){
+      continue;
+    }
+
+    cpu_ram_write(cpu, address++, bc);
+  }
+
+  fclose(fp);
+
   // char data[DATA_LEN] = {
   //   // From print8.ls8
   //   0b10000010, // LDI R0,8
@@ -44,6 +66,7 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
   switch (op) {
     case ALU_MUL:
       // TODO
+      cpu->registers[regA] *= cpu->registers[regB];
       break;
 
     // TODO: implement more ALU ops
@@ -60,13 +83,22 @@ void cpu_run(struct cpu *cpu)
 
   while (running) {
       unsigned char IR = cpu_ram_read(cpu, cpu->PC); 
-      unsigned char operandA = cpu_ram_read(cpu, cpu->PC + 1);
-      unsigned char operandB = cpu_ram_read(cpu, cpu->PC + 2); 
+      unsigned char operandA = cpu_ram_read(cpu, (cpu->PC + 1) & 0xff);
+      unsigned char operandB = cpu_ram_read(cpu, (cpu->PC + 2) & 0xff); 
     // TODO
     // 1. Get the value of the current instruction (in address PC).
     // 2. switch() over it to decide on a course of action.
     // 3. Do whatever the instruction should do according to the spec.
     // 4. Move the PC to the next instruction.
+
+    // printf("TRACE: %02X | %02X %02X %02X |", cpu->PC, IR, operandA, operandB);
+
+    // for(int i = 0; i < 8; i++) {
+    //   printf("%02X", cpu->registers[i]);
+    // }
+
+    // printf("\n");
+
     switch(IR) {
       case LDI:
         cpu->registers[operandA] = operandB;
@@ -76,6 +108,11 @@ void cpu_run(struct cpu *cpu)
       case PRN:
         printf("%d\n", cpu->registers[operandA]);
         cpu->PC += 2;
+        break;
+
+      case MUL:
+        alu(cpu, ALU_MUL, operandA, operandB);
+        cpu->PC += 3;
         break;
 
       case HLT:
