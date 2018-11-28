f6c29ef7c937a86821d3dd5b6b3383a3464aa676
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-11-28 17:39:01 -0500

    completed day 4

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -11,28 +11,24 @@
  */
 void cpu_load(struct cpu *cpu, char *filename)
 {
-  // char data[DATA_LEN] = {
-  //   // From print8.ls8
-  //   0b10000010, // LDI R0,8
-  //   0b00000000,
-  //   0b00001000,
-  //   0b01000111, // PRN R0
-  //   0b00000000,
-  //   0b00000001  // HLT
-  // };
   FILE *fp = fopen(filename, "r");
   int address = 0;
-  char c[8192];
-  while (fgets(c, sizeof c, fp) != NULL) {
-   cpu->ram[address++] = strtoul(c, NULL, 2);
+  char line[32];
+  char *ptr;
+  while (fgets(line, sizeof line, fp) != NULL) {
+    cpu->ram[address++] = strtoul(line, &ptr, 2);
+    #if DEBUG
+      printf("Line that is not used: %s\n", ptr);
+      #endif
   }
   fclose(fp);
+  #if DEBUG
+    for (int i = 0; i < 64; i++) {
+      printf("RAM at index %d: %d\n", i, cpu->ram[i]);
+    }
+    #endif
+  
 
- 
-
- 
-
-  // TODO: Replace this with something less hard-coded
 }
 
 /**
@@ -44,6 +40,11 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
     case ALU_MUL:
       cpu->registers[regA] = cpu->registers[regA] * cpu->registers[regB];
       break;
+    case ALU_ADD:
+      cpu->registers[regA] = cpu->registers[regA] + cpu->registers[regB];
+      break;
+    default:
+      printf("Default case reached. No ALU function triggered.\n");
 
     // TODO: implement more ALU ops
   }
@@ -73,51 +74,89 @@ void cpu_run(struct cpu *cpu)
     unsigned char IR = cpu_ram_read(cpu, PC);
     unsigned char operandA = cpu_ram_read(cpu, PC+1);
     unsigned char operandB = cpu_ram_read(cpu, PC+2);
+    int INC_PC = 0;
     // 2. switch() over it to decide on a course of action.
     // 3. Do whatever the instruction should do according to the spec.
     switch(IR) {
       case HLT:
-        printf("HLT\n");
+        #if DEBUG
+          printf("HLT\n");
+          #endif
         running = 0;
+        INC_PC = 1;
         break;
       case LDI:
-        printf("LDI\n");
-        cpu->registers[(int)operandA] = operandB;
+        #if DEBUG
+          printf("LDI\n");
+          #endif
+        cpu->registers[operandA] = operandB;
+        INC_PC = 1;
         break;
       case PRN:
-        printf("PRN\n");
-        printf("%d\n", cpu->registers[(int)operandA]);
+        #if DEBUG
+          printf("RET\n");
+          #endif
+        printf("%d\n", cpu->registers[operandA]);
+        INC_PC = 1;
         break;
       case MUL:
-        printf("MUL\n");
-        // cpu->registers[(int)operandA] =  cpu->registers[(int)operandA] *  cpu->registers[(int)operandB];
+        #if DEBUG
+          printf("MUL\n");
+          #endif
         alu(cpu, ALU_MUL, operandA, operandB);
+        INC_PC = 1;
         break;
-        // default:
-        // printf("%d", IR);
-        // printf("Default case reached.\n");
-        // break;
-        case PUSH:
-        if(DEBUG) {
+      case PUSH:
+        #if DEBUG
           printf("PUSH\n");
-        }
+          #endif
         cpu->ram[--cpu->registers[7]] = cpu->registers[operandA];
+        INC_PC = 1;
         break;
-        case POP:
-        if (DEBUG) {
+      case POP:
+        #if DEBUG
           printf("POP\n");
-        }
+          #endif
         cpu->registers[operandA] = cpu->ram[cpu->registers[7]++];
+        INC_PC = 1;
         break;
-        default:
+      case CALL:
+        #if DEBUG
+          printf("CALL\n");
+          #endif
+        cpu->ram[--cpu->registers[7]] = PC+2;
+        cpu->PC = cpu->registers[operandA] + 1;
+        #if DEBUG
+          printf("What we're setting PC to in CALL: %d\n", cpu->registers[operandA]);
+          #endif
+        INC_PC = 0;
+        break;
+      case RET:
+        #if DEBUG
+          printf("RET\n");
+          #endif
+        cpu->PC = cpu->ram[cpu->registers[7]++];
+        INC_PC = 0;
+        break;
+      case ADD:
+        #if DEBUG 
+          printf("ADD\n");
+          #endif
+        alu(cpu, ALU_ADD, operandA, operandB);
+        INC_PC = 1;
+        break;
+      default:
         printf("Instruction number: %d\n", IR);
         printf("Default case reached. Command invalid.\n");
         break;
-
     }
     // 4. Move the PC to the next instruction.
-    // printf("%d\n", (instruction >> 6));
-    cpu->PC += 1 + (int)(IR >> 6);
+    if (INC_PC) {
+      cpu->PC += 1 + (IR >> 6);
+    }
+    #if DEBUG
+      sleep(1);
+      #endif
     
   }
 }
@@ -135,4 +174,8 @@ void cpu_init(struct cpu *cpu)
   cpu->FL = 0;
   memset(cpu->ram, 0, 256);
 }
+
+
+
+
 
