4939fdf8a77e16a3db22f2ca18d274cb61ad6cb8
tulf <timzulf@gmail.com>
2018-11-28 17:48:40 -0500

    Added instructions to cpu.h cpu.c: ALU opps,cpu run logic with stack  queue Program cimpiles now but can't read instuctions properly pretty suer I set them up correclty in cpu.h, somewhat at a loss about what to do now tbh.

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,39 +1,49 @@
 // header file to make access to CPU/ALU functions:
 #include "cpu.h"
-
+// include other stuff:
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define DATA_LEN 6
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-unsigned char cpu_ram_read(struct cpu *cpu, int index){
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char index){
   // unsigned because we need a return for the read
   return cpu->ram[index];
 }
-void cpu_ram_write(struct cpu *cpu, int index, unsigned char *value){
+void cpu_ram_write(struct cpu *cpu, unsigned char index, unsigned char value){
   // assign value to array element at given index.
-  cpu->ram[index]=*value;
+  cpu->ram[index] = value;
 }
 
-void cpu_load(struct cpu *cpu)
+// add args to cpuload to use FILE I/O:
+void cpu_load(struct cpu *cpu, char *argv[])
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
-
-  int address = 0;
-
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  FILE *fp;
+  // make data into a big array
+  char data[1024];
+  //initialize memory address:
+  unsigned char address = 0;
+  //open file as read only to input instructions
+  fp = fopen(argv[1], "r");
+  //check if the file doesn't exist.
+  if (fp == NULL){
+    perror("file can't be opened");
+    exit(1);
   }
 
-  // TODO: Replace this with something less hard-coded
+  while(fgets(data, sizeof(data), fp) != NULL){
+    //convert data to long unsigned integer
+    unsigned char byte = strtoul(data, NULL, 2);
+
+    if(data == NULL){
+      continue;
+    }
+    cpu->ram[address++] = byte;
+  }
+  fclose(fp);
 }
 
 /**
@@ -43,10 +53,26 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 {
   switch (op) {
     case ALU_MUL:
-      // TODO
+      // notice this overwrites whatevr registeer regA is as the product:
+      regist[regA] *= regist[regB];
       break;
 
     // TODO: implement more ALU ops
+    case ALU_ADD:
+      regist[regA] += regist[regB];
+      break;
+    // TODO: implement more ALU ops
+    case ALU_SUB:
+      regist[regA] -= regist[regB];
+      break;
+      // TODO: implement more ALU ops
+    case ALU_DIV:
+      regist[regA] /= regist[regB];
+      break;
+    // TODO: implement more ALU ops
+    case ALU_MOD:
+      regist[regA] = regist[regA]%regist[regB];
+      break;
   }
 }
 
@@ -58,6 +84,12 @@ void cpu_run(struct cpu *cpu)
   int running = 1; // True until we get a HLT instruction
 
   while (running) {
+    int c = ram[PC];
+    switch (c) {
+      case HLT:
+      running = 0;
+      break;
+    }
     // TODO
     // 1. Get the value of the current instruction (in address PC).
     // 2. switch() over it to decide on a course of action.
@@ -72,9 +104,10 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
-  cpu->PC = 0;
-  cpu->ram = calloc(256, sizeof(unsigned char))
+  cpu->PC = 0x00;
+  cpu->regist[SP] = 0xF4;
+  //cpu->ram = calloc(256, sizeof(unsigned char));
   // TODO: Zero registers and RAM
-  memset(cpu->registers, 0, sizeof(cpu->registers));
+  memset(cpu->regist, 0, sizeof(cpu->regist));
   memset(cpu->ram, 0, sizeof(cpu->ram));
 }
