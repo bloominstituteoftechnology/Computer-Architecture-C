62b82a685cb917ac5922268c3d841bbd6c9935c9
sca4cs <shannon.atkinson.ca@gmail.com>
2018-11-29 18:14:48 -0500

    attempted stretch

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -66,6 +66,18 @@ void cpu_run(struct cpu *cpu)
   int running = 1; // True until we get a HLT instruction
 
   while (running) {
+    
+    if (cpu->registers[IM] == 1) {
+      unsigned char interrupts = cpu->registers[IM] & cpu->registers[IS];
+      for (int i = 0; i < 8; i++) {
+        // Right shift interrupts down by i, then mask with 1 to see if that bit was set
+        int interrupt_happened = ((interrupts >> i) & 1) == 1;
+        // ...
+      }
+    }
+    // call gettimeofday() and check to see if one second has passed
+    // if yes, set bit 0 of R6 (interrupt status)
+
     unsigned char IR = cpu_ram_read(cpu, cpu->PC);  // Get the value of the current instruction (in address PC)
     int move_pc = (IR >> 6) + 1;  // move counter
 
@@ -98,26 +110,60 @@ void cpu_run(struct cpu *cpu)
         break;
 
       case PUSH:
-        cpu->registers[7] --;
-        cpu_ram_write(cpu, cpu->registers[7], cpu->registers[operandA]);
+        cpu->registers[SP] --;
+        cpu_ram_write(cpu, cpu->registers[SP], cpu->registers[operandA]);
         cpu->PC += move_pc;
         break;
 
       case POP:
-        cpu->registers[operandA] = cpu_ram_read(cpu, cpu->registers[7]);
-        cpu->registers[7] ++;
+        cpu->registers[operandA] = cpu_ram_read(cpu, cpu->registers[SP]);
+        cpu->registers[SP] ++;
         cpu->PC += move_pc;
         break;
 
-      case CALL:
-        cpu->registers[7] --;
-        cpu_ram_write(cpu, cpu->registers[7], cpu->PC + 2);
+      case CALL:  //Calls a subroutine (function) at the address stored in the register.
+        cpu->registers[SP] --;
+        cpu_ram_write(cpu, cpu->registers[SP], cpu->PC + 2);
         cpu->PC = cpu->registers[operandA];
         break;
 
-      case RET:
-        cpu->PC = cpu_ram_read(cpu, cpu->registers[7]);
-        cpu->registers[7] ++;
+      case RET:  //Return from subroutine.
+        cpu->PC = cpu_ram_read(cpu, cpu->registers[SP]);
+        cpu->registers[SP] ++;
+        break;
+
+      case ST:  //Store value in registerB in the address stored in registerA.
+        cpu_ram_write(cpu, cpu->registers[operandA], cpu->registers[operandB]);
+        cpu->PC += move_pc;
+        break;
+
+      case JMP:  //Jump to the address stored in the given register.
+        cpu->PC = cpu->registers[operandA];
+        break;
+
+      case PRA:  //Print alpha character value stored in the given register.
+        printf("%c", cpu->registers[operandA]);
+        cpu->PC += move_pc;
+        break;
+
+      case INT:  //Issue the interrupt number stored in the given register.
+        //This will set the _n_th bit in the `IS` register to the value in the given register.
+        break;
+
+      case IRET:  //Return from an interrupt handler.
+        // The following steps are executed:
+        // 1. Registers R6-R0 are popped off the stack in that order.
+        for (int i = 6; i >= 0; i--) {
+          cpu->registers[i] = cpu_ram_read(cpu, cpu->registers[SP]);
+          cpu->registers[SP] ++;
+        }
+        // 2. The `FL` register is popped off the stack.
+        cpu->FL = cpu_ram_read(cpu, cpu->registers[SP]);
+        cpu->registers[SP] ++;
+        // 3. The return address is popped off the stack and stored in `PC`.
+        cpu->PC = cpu_ram_read(cpu, cpu->registers[SP]);
+        cpu->registers[SP] ++;
+        // 4. Interrupts are re-enabled
         break;
 
       default:
@@ -133,9 +179,10 @@ void cpu_init(struct cpu *cpu)
 {
   // Initialize the PC and other special registers
   cpu->PC = 0;
+  cpu->FL = 0;
   // Zero registers and RAM
   memset(cpu->registers, 0, sizeof(cpu->registers));
   memset(cpu->ram, 0, sizeof(cpu->ram));
 
-  cpu->registers[7] = 0xF4;
+  cpu->registers[SP] = 0xF4;
 }
--- ls8/cpu.h
+++ ls8/cpu.h
@@ -4,6 +4,7 @@
 // Holds all information about the CPU
 struct cpu {
   int PC;  // PC
+  unsigned char FL;
   unsigned char registers[8];  // registers (array)
   unsigned char ram[256];  // ram (array)
 };
@@ -17,6 +18,9 @@ enum alu_op {
 // Instructions
 // These use binary literals. If these aren't available with your compiler, hex
 // literals should be used.
+#define SP 7  // stack pointer
+#define IM 5  // stack pointer
+#define IS 6  // stack pointer
 #define LDI  0b10000010
 #define HLT  0b00000001
 #define PRN  0b01000111
@@ -26,6 +30,11 @@ enum alu_op {
 #define POP  0b01000110
 #define CALL  0b01010000
 #define RET  0b00010001
+#define ST  0b10000100
+#define JMP  0b01010100
+#define PRA  0b01001000
+#define IRET  0b00010011
+#define INT  0b01010010
 
 // Function declarations
 extern void cpu_load(struct cpu *cpu, char *file);
