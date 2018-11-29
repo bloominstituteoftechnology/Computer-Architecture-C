0a0c0acf2dfe3c1a8d5a6076b851a277153c10e6
Sania Parekh <sparekh@Sanias-MacBook.local>
2018-11-29 14:16:11 -0600

    RAM functions

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -65,3 +65,11 @@ void cpu_init(struct cpu *cpu)
 
   // TODO: Zero registers and RAM
 }
+
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char index) {
+  return cpu->ram[index];
+}
+
+void cpu_ram_write(struct cpu *cpu, unsigned char index, unsigned char value) {
+  cpu->ram[index] = value;
+}
