5b25f778506ce46aeb07002d0616fc53d266d27d
Sukhada Gholba <sukhadagholba@gmail.com>
2018-11-30 10:33:49 -0800

    added JMP, JEQ, JNE, and CMP functionality

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -30,7 +30,8 @@ void cpu_load(struct cpu *cpu, char *argv[])
 	FILE *fp;
 	fp = fopen(argv[1], "r");
 	char line[1024];
-	unsigned char address = 0;
+	int address = 0;
+	char *data;
 
 	if(fp == NULL){
 		fprintf(stderr, "Cannot open the file %s \n", argv[1]);
@@ -40,14 +41,14 @@ void cpu_load(struct cpu *cpu, char *argv[])
 	while(fgets(line, sizeof(line), fp)!= NULL){
 		        
 			//char *endptr;
-   			unsigned char byte = strtoul(line, NULL, 2);
+   			unsigned char byte = strtoul(line, &data, 2);
 			
-			if(line == NULL){
+			if(data == line){
         			continue;
       			}
       			
 			//printf("CPU Load-writing to RAM %d at %d \n", byte, address);
-			cpu->ram[address++] = byte;
+		        cpu->ram[address++] = byte;
     	}
 	
 	fclose(fp);
@@ -80,13 +81,36 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
       //printf("Adding....in ALU_\n");
       break;
 
-    case ALU_SUB:
+    case ALU_CMP:
+      if(cpu->reg[regA] == cpu->reg[regB]){
+      	cpu->equal = 1;
+      }  
+      else {
+      	cpu->equal = 0;
+      }
+
+      if(cpu->reg[regA] > cpu->reg[regB]){
+        cpu->greater = 1;
+      }
+      else {
+        cpu->greater = 0;
+      }
+
+      if(cpu->reg[regA] < cpu->reg[regB]){
+        cpu->less = 1;
+      }
+      else {
+        cpu->less = 0;
+      }
+      break;
+
+      case ALU_SUB:
       cpu->reg[regA] = cpu->reg[regA] - cpu->reg[regB];
       break;
-    
-    default:
+
+     default:
         printf("Unrecognized instruction");
-        exit(1);  
+        exit(1); 
 
   }
 }
@@ -120,28 +144,31 @@ void cpu_run(struct cpu *cpu)
 
 		case LDI:
 		cpu->reg[operandA] = operandB;
-		//printf("Loading... \n");
+		//printf("LDI... \n");
+		PC += shift;
 		break;
 
       		case PRN:
         	printf("%d \n", cpu->reg[operandA]);
+		PC += shift;
 		break;
 		
 		case PUSH:
 		//printf("Pushing value %d to Stack \n", cpu->reg[operandA]);
 		cpu_ram_write(cpu, --cpu->reg[SP], cpu->reg[operandA]);
+		PC += shift;
 		break;
 
 		case POP:
 		cpu->reg[operandA] = cpu_ram_read(cpu, cpu->reg[SP]++);
 		//printf("Popping value %d from Stack \n", cpu->reg[operandA]);
+		PC += shift;
 		break;
 
 		case CALL:
         	cpu->reg[SP]--;
         	cpu_ram_write(cpu, cpu->reg[SP], PC + 2);
         	PC = cpu->reg[operandA];
-        	shift = 0;
 		//printf("CALL PC value is: %d \n", PC);
 		break;
       
@@ -152,29 +179,65 @@ void cpu_run(struct cpu *cpu)
 		//printf("Return \n");
 		break;
 
+		case CMP:
+		//printf("cmp \n");
+        	alu(cpu, ALU_CMP, operandA, operandB);
+        	PC += shift;
+		break;
+
+		case JMP:
+		//printf("jump \n");
+		//--cpu->reg[SP];
+		PC = cpu->reg[operandA];
+		break;
+
+		case JNE:
+		if(cpu->equal == 0){
+			  PC = cpu->reg[operandA];
+		}
+		else{
+			PC += shift;
+		}
+		//printf("JNE \n");
+		break;
+
+		case JEQ:
+		//printf("JEQ \n");
+		if(cpu->equal == 1){
+                          PC = cpu->reg[operandA];
+                }
+		else {
+			PC += shift;
+		}
+                break;
+
 		case MUL:
       		alu(cpu, ALU_MUL, operandA, operandB);
+		PC += shift;
 		break;
 
 		case DIV:
 		alu(cpu, ALU_DIV, operandA, operandB);
+		PC += shift;
 		break;
 
     		case ADD:
       		alu(cpu, ALU_ADD, operandA, operandB);
 		//printf("Adding..\n");
+		PC += shift;
 		break;
 
 		case SUB:
                 alu(cpu, ALU_SUB, operandA, operandB);
+		PC += shift;
 		break;
 		
-		/*default:
+		default:
         	printf("Unknown instruction %02x: %02x\n", PC, IR);
-        	exit(1);*/
+        	exit(1);
 
 	}	
-		 PC += shift;
+		 //PC += shift;
   }
 }
 
@@ -189,6 +252,10 @@ void cpu_init(struct cpu *cpu)
    cpu->PC = 0x00;
    
    cpu->reg[SP] = 0xF4;   //setting SP to higher address
+
+   cpu->equal = 0x00;   //  Initializing flags
+   cpu->greater = 0x00;
+   cpu->less = 0x00;   
 
    memset(cpu->reg, 0, sizeof(cpu->reg));
    memset(cpu->ram, 0, sizeof(cpu->ram));
