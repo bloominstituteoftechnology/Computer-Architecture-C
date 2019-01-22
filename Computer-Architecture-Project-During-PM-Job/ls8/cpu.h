c1e317e747425d03c0c87a0eb4d0f036235e7554
Adrian Adames <adrianadames@gmail.com>
2019-01-22 15:48:50 -0500

    Added copy of original computer architecture project repo to this folder. Will redo this project for practice and to test my recollection of the material.

---
+++ Computer-Architecture-Project-During-PM-Job/ls8/cpu.h
@@ -0,0 +1,34 @@
+#ifndef _CPU_H_
+#define _CPU_H_
+
+// Holds all information about the CPU
+struct cpu {
+  // TODO
+  // PC
+  // registers (array)
+  // ram (array)
+};
+
+// ALU operations
+enum alu_op {
+	ALU_MUL
+	// Add more here
+};
+
+// Instructions
+
+// These use binary literals. If these aren't available with your compiler, hex
+// literals should be used.
+
+#define LDI  0b10000010
+#define HLT  0b00000001
+#define PRN  0b01000111
+// TODO: more instructions here. These can be used in cpu_run().
+
+// Function declarations
+
+extern void cpu_load(struct cpu *cpu);
+extern void cpu_init(struct cpu *cpu);
+extern void cpu_run(struct cpu *cpu);
+
+#endif
