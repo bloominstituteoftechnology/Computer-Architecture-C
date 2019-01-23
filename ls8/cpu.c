8951af9b624868f346f19b9d9c5ebe46b328b80e
jaymzsocool <james.mcraejr@gmail.com>
2019-01-23 16:13:32 -0500

    Unharcoded completed

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,29 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "cpu.h"
 
-#define DATA_LEN 6
+// #define DATA_LEN 6
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char* file)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+  // char data[DATA_LEN] = {
+  //   // From print8.ls8
+  //   0b10000010, // LDI R0,8
+  //   0b00000000,
+  //   0b00001000,
+  //   0b01000111, // PRN R0
+  //   0b00000000,
+  //   0b00000001  // HLT
+  // };
 
-  int address = 0;
+  // int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
-  }
+  // for (int i = 0; i < DATA_LEN; i++) {
+  //   cpu->ram[address++] = data[i];
+  // }
 
   // TODO: Replace this with something less hard-coded
+  FILE *fp = fopen(file,"r");
+  if (fp == NULL)
+  {
+    printf("ERROR: Could not open file.");
+  }
+  int address = 0;
+  char string [256];
+  while (fgets(string, sizeof(string),fp) != NULL)
+  {
+    unsigned char data = strtol(string, NULL, 2);
+    cpu->ram[address++] = data;
+  }
+  fclose(fp);
 }
 
 unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address)
--- ls8/ls8.c
+++ ls8/ls8.c
@@ -4,13 +4,21 @@
 /**
  * Main
  */
-int main(void)
+int main(int argc, char *argv[])
 {
   struct cpu cpu;
 
-  cpu_init(&cpu);
-  cpu_load(&cpu);
-  cpu_run(&cpu);
+  if (argc > 1)
+  {
+    cpu_init(&cpu);
+    cpu_load(&cpu, argv[1]);
+    cpu_run(&cpu);
+  }
+  else
+  {
+    printf("Must specify file to run.");
+  }
+  
 
   return 0;
 }
