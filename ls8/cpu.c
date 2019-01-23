9c8bc7a81baa6d6b779c858df953479f388b9519
Brian Ruff <thelonewolf198927@gmail.com>
2019-01-23 12:43:40 -0500

    update lang

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -52,20 +52,10 @@ void cpu_run(struct cpu *cpu)
   unsigned char operandB;
 
   while (running) {
-    unsigned char IR = cpu_ram_read(cpu, cpu->PC);
-    unsigned int num_of_operands = IR >> 6;
     // TODO
     // 1. Get the value of the current instruction (in address PC).
     // 2. Figure out how many operands this next instruction requires
-    if(num_of_operands == 2)
-    {
-      operandA = cpu_ram_read(cpu, (cpu->PC + 1) &0xff);
-      operandB = cpu_ram_read(cpu, (cpu->PC + 2) &0xff);
-    } else if (num_of_operands = 1){
-      operandA = cpu_ram_read(cpu, (cpu->PC + 1) &0xff);
-    }
     // 3. Get the appropriate value(s) of the operands following this instruction
-    int instruction_set_PC = (IR >> 4) &1;
     // 4. switch() over it to decide on a course of action.
     // 5. Do whatever the instruction should do according to the spec.
     // 6. Move the PC to the next instruction.
