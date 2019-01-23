1e1f79f239db22b3fb7d7d25da9e86219a1fe12e
Brian Ruff <thelonewolf198927@gmail.com>
2019-01-22 19:43:39 -0500

    cpu run, in progress

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -46,12 +46,26 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
+  unsigned char *reg = cpu->reg;
+  unsigned char *ram = cpu->ram;
+  unsigned char operandA;
+  unsigned char operandB;
 
   while (running) {
+    unsigned char IR = cpu_ram_read(cpu, cpu->PC);
+    unsigned int num_of_operands = IR >> 6;
     // TODO
     // 1. Get the value of the current instruction (in address PC).
     // 2. Figure out how many operands this next instruction requires
+    if(num_of_operands == 2)
+    {
+      operandA = cpu_ram_read(cpu, (cpu->PC + 1) &0xff);
+      operandB = cpu_ram_read(cpu, (cpu->PC + 2) &0xff);
+    } else if (num_of_operands = 1){
+      operandA = cpu_ram_read(cpu, (cpu->PC + 1) &0xff);
+    }
     // 3. Get the appropriate value(s) of the operands following this instruction
+    int instruction_set_PC = (IR >> 4) &1;
     // 4. switch() over it to decide on a course of action.
     // 5. Do whatever the instruction should do according to the spec.
     // 6. Move the PC to the next instruction.
