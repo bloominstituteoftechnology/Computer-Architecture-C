aa58da1869b0117fc43c5776749405824cef1d44
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-01-24 18:34:40 -0500

    mult.ls8 && stack.ls8

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -4,88 +4,117 @@
 
 #define DATA_LEN 6
 
-
 // Write the given value to the LS8's RAM at the given address
-void cpu_ram_write(struct cpu *cpu, unsigned char value, unsigned char address) {
-  cpu->ram[address] = value;
+void cpu_ram_write(struct cpu *cpu, unsigned char MDR, unsigned char MAR) {
+  cpu->ram[MAR] = MDR;
 }
 
 // Read the value at address and return it
-unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address) {
-  return cpu->ram[address];
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char MAR) {
+  return cpu->ram[MAR];
 }
+
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, unsigned char *argv)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+  FILE *fp;
+  char line[1024];
 
+  // Initialize RAM address to start writing to
   int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  // Open the source file
+  if ((fp = fopen(argv, "r")) == NULL) {
+    fprintf(stderr, "Cannot open %s\n", argv);
+    exit(1);
   }
 
-  // TODO: Replace this with something less hard-coded
+  // Read in the source file line by line
+  while(fgets(line, sizeof(line), fp) != NULL) {
+
+    // convert these to binary strings to numbers
+    char *ptr;
+    unsigned char ret = strtol(line, &ptr, 2);
+
+    // Ignore lines from which no numbers were read
+    if (ptr == line) {
+      continue;
+    }
+
+    // Write to RAM
+    // Increment RAM address by how much was written to it
+    cpu->ram[address++] = ret;
+  }
+  fclose(fp);
 }
+
 /**
  * ALU
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
+  unsigned char *reg = cpu->reg;
+
   switch (op) {
     case ALU_MUL:
-      // TODO
+      reg[regA] *= reg[regB];
       break;
 
     // TODO: implement more ALU ops
   }
 }
 
-
-
 /**
  * Run the CPU
  */
 void cpu_run(struct cpu *cpu)
 {
-  int running = 1; // True until we get a HLT instruction
+  int running = 1; 
 
   while (running) {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
     unsigned char IR = cpu_ram_read(cpu, cpu->PC);
     // 2. Figure out how many operands this next instruction requires
-
-    // 3. Get the appropriate value(s) of the operands following this instruction
     unsigned char operandA = cpu_ram_read(cpu, cpu->PC + 1);
     unsigned char operandB = cpu_ram_read(cpu, cpu->PC + 2);
+
+
     // 4. switch() over it to decide on a course of action.
     // 5. Do whatever the instruction should do according to the spec.
     switch(IR) {
       case HLT:
+        // Break the loop
         running = 0;
         break;
       case LDI:
+        // set the current operation to the next operation
         cpu->reg[operandA] = operandB;
         break;
       case PRN:
+        // Print the value at the given register
         printf("%d\n", cpu->reg[operandA]);
+        break;
+      case MUL:
+        // Multiple two numbers together
+        alu(cpu, ALU_MUL, operandA, operandB);
+        break;
+      case POP:
+        // Remove item from stack
+        cpu_ram_write(cpu, cpu->reg[ptr], cpu->reg[SP]++);
+        break;
+      case PUSH:
+        // Add item to stack
+        cpu_ram_write(cpu, cpu->reg[ptr], --cpu->reg[SP]);
         break;
 
     }
 
     // 6. Move the PC to the next instruction.
     cpu->PC += (IR >> 6) + 1;
+
   }
 }
 
@@ -95,9 +124,8 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
-  cpu->PC = 0;
+  cpu->PC = ADDR_PROGRAM_ENTRY;
+  cpu->reg[SP] = ADDR_EMPTY_STACK;
   memset(cpu->reg, 0, sizeof(cpu->reg));
   memset(cpu->ram, 0, sizeof(cpu->ram));
 }
-
-//////
