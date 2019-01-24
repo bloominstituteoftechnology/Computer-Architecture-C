10eb1e6d410e6a6de7b7433f592cec223f162d29
Brian Ruff <thelonewolf198927@gmail.com>
2019-01-24 18:56:47 -0500

    cpu.c and cpu.h files done

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,8 +1,7 @@
-#include "cpu.h"
-#include <string.h>
 #include <stdio.h>
-
-#define DATA_LEN 6
+#include <stdlib.h>
+#include <string.h>
+#include "cpu.h"
 
 /**
  * Helper function to write a value to the specified index in RAM
@@ -22,25 +21,57 @@ unsigned char cpu_ram_read(struct cpu *cpu, unsigned char index)
 }
 
 /**
+ * Push a value on the CPU stack
+ */
+void cpu_push(struct cpu *cpu, unsigned char val)
+{
+  cpu->reg[SP]--;
+
+  cpu_ram_write(cpu, val, cpu->reg[SP]);
+}
+
+/**
+ * Pop a value from the CPU stack
+ */
+unsigned char cpu_pop(struct cpu *cpu)
+{
+  unsigned char val = cpu_ram_read(cpu, cpu->reg[SP]);
+
+  cpu->reg[SP]++;
+
+  return val;
+}
+
+/**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(char *file_name, struct cpu *cpu)
+void cpu_load(char *filename, struct cpu *cpu)
 {
   FILE *fp;
-  int address = ADDR_PROGRAM_ENTRY;
   char line[1024];
+  int address = ADDR_PROGRAM_ENTRY;
 
-  if((fp = fopen("print8.ls8", "r")) == NULL) {
-    fprintf(stderr, "Cannot open this file >> %s", file_name);
-  };
+  // Open the source file
+  if ((fp = fopen(filename, "r")) == NULL) {
+    fprintf(stderr, "Cannot open file %s\n", filename);
+    exit(2);
+  }
 
-  while(fgets(line, sizeof(line), fp) != NULL){
+  // Read all the lines and store them in RAM
+  while (fgets(line, sizeof line, fp) != NULL) {
+
+    // Convert string to a number
     char *endchar;
-    unsigned char byte = strtol(line, &endchar, 2);
-    if(endchar == line) continue;
-    cpu_ram_write(cpu, byte, address++);
-  };
+    unsigned char byte = strtol(line, &endchar, 2);;
 
+    // Ignore lines from which no numbers were read
+    if (endchar == line) {
+      continue;
+    }
+
+    // Store in ram
+    cpu_ram_write(cpu, byte, address++);
+  }
 }
 
 /**
@@ -48,12 +79,39 @@ void cpu_load(char *file_name, struct cpu *cpu)
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
+  unsigned char *reg = cpu->reg;
+
+  unsigned char valB = reg[regB];
+
   switch (op) {
     case ALU_MUL:
-      // TODO
+      reg[regA] *= valB;
       break;
 
-    // TODO: implement more ALU ops
+    case ALU_ADD:
+      reg[regA] += valB;
+      break;
+
+    case ALU_INC:
+      reg[regA]++;
+      break;
+
+    case ALU_DEC:
+      reg[regA]--;
+      break;
+
+    case ALU_CMP:
+      if (reg[regA] == valB) {
+        // Set the last bit of FL to 1
+        cpu->FL = cpu->FL | (1 << 0);
+      } else if (reg[regA] > valB) {
+        // Set the second-to-last bit of FL to 1
+        cpu->FL = cpu->FL | (1 << 1);
+      } else {
+        // Set the third-to-last bit of FL to 1
+        cpu->FL = cpu->FL | (1 << 2);
+      }
+      break;
   }
 }
 
@@ -101,8 +159,42 @@ void cpu_run(struct cpu *cpu)
         printf("%d\n", reg[operandA]);
         break;
 
+      case MUL:
+        alu(cpu, ALU_MUL, operandA, operandB);
+        break;
+
+      case ADD:
+        alu(cpu, ALU_ADD, operandA, operandB);
+        break;
+
       case HLT:
         running = 0;
+        break;
+
+      case PRA:
+        printf("%c\n", reg[operandA]);
+        //printf("%c", reg[operandA]); fflush(stdout); // Without newline
+        break;
+
+
+      case PUSH:
+        cpu_push(cpu, reg[operandA]);
+        break;
+
+      case POP:
+        reg[operandA] = cpu_pop(cpu);
+        break;
+
+      case INC:
+        alu(cpu, ALU_INC, operandA, operandB);
+        break;
+
+      case DEC:
+        alu(cpu, ALU_DEC, operandA, operandB); 
+        break;
+
+      case CMP:
+        alu(cpu, ALU_CMP, operandA, operandB);
         break;
 
       default:
@@ -127,8 +219,13 @@ void cpu_run(struct cpu *cpu)
  */
 void cpu_init(struct cpu *cpu)
 {
-  // TODO: Initialize the PC and other special registers
   cpu->PC = 0;
-  memset(cpu->ram, 0, sizeof(cpu->reg));
-  memset(cpu->reg, 0, sizeof(cpu->ram));
+  cpu->FL = 0;
+
+  // Zero registers and RAM
+  memset(cpu->reg, 0, sizeof cpu->reg);
+  memset(cpu->ram, 0, sizeof cpu->ram);
+
+  // Initialize SP
+  cpu->reg[SP] = ADDR_EMPTY_STACK;
 }
