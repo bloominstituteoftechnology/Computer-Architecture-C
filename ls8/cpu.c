ebbbfac295c7df11e10d01bfad21afb2e2b6348e
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2019-01-24 18:08:42 -0800

    finished day one (implemented LDI, PRN, HLT, cpu_run(), struct cpu, cpu ram functions )

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,7 +1,23 @@
 #include "cpu.h"
-
+#include <string.h>
+#include <stdio.h>
 
 #define DATA_LEN 6
+
+/*
+  RAM interface functions
+*/
+
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address)
+{
+ return cpu->ram[address];
+}
+
+void cpu_ram_write(struct cpu *cpu, unsigned char value, unsigned char address)
+{
+  cpu->ram[address] = value;
+}
+
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
@@ -48,14 +64,40 @@ void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
+  unsigned char operand_a;
+  unsigned char operand_b;
+
   while (running) {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
+    unsigned char IR = cpu->ram[cpu->PC];
     // 2. Figure out how many operands this next instruction requires
+    unsigned int number_of_operands = (IR >> 6);
     // 3. Get the appropriate value(s) of the operands following this instruction
+    if (number_of_operands == 2) {
+      operand_a = cpu->ram[cpu->PC+1];
+      operand_b = cpu->ram[cpu->PC+2];
+    } else if (number_of_operands == 1) {
+      operand_a = cpu->ram[cpu->PC+1];
+    } else {
+
+    }
     // 4. switch() over it to decide on a course of action.
+    switch (IR) {
+      case LDI:
+        cpu->reg[operand_a] = operand_b;
+        break;
+      case PRN:
+        printf("%d\n", cpu->reg[operand_a]);
+        break;
+      case HLT:
+        running = 0;
+      default:
+        break;
+    }
     // 5. Do whatever the instruction should do according to the spec.
     // 6. Move the PC to the next instruction.
+    cpu->PC = cpu->PC + number_of_operands + 1;
   }
 }
 
@@ -65,4 +107,9 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
+  memset(cpu->ram, 0, 256);
+
+  cpu->PC = 0;
+
+  memset(cpu->reg, 0, 8);
 }
