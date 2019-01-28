eb8d4cd4b92a7f216a4deda5186bc6d19fb18d81
MichelleOk <umbralcompendium@gmail.com>
2019-01-28 16:51:23 -0700

    day 1 done, with notes

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,6 +1,19 @@
 #include "cpu.h"
+#include <stdio.h>
+#include <string.h>
+
 
 #define DATA_LEN 6
+
+// returns the byte we are reading from the cpu struct, with the address = memory address register (mar) 
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char mar) {
+  return cpu->ram[mar];
+}
+// mdr = memory data register
+void cpu_ram_write(struct cpu *cpu, unsigned char mar, unsigned char mdr) {
+  cpu->ram[mar] = mdr;
+}
+// "read" returns the value, "write" sets the value in the array.
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
@@ -46,13 +59,55 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
+  // declare instruction run variable, operandA and operandB are bytes PC1 and PC2
+  unsigned char IR, operandA, operandB;
 
   while (running) {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
+    // bring in the read function, get the instruction at that address
     // 2. Figure out how many operands this next instruction requires
     // 3. Get the appropriate value(s) of the operands following this instruction
     // 4. switch() over it to decide on a course of action.
+
+    // PC = index of current executing in struction in RAM array, +1,+2 are the next index after
+    // & 0xff = ??? suppose to help with a bug
+    IR = cpu_ram_read(cpu, cpu->PC);
+    operandA = cpu_ram_read(cpu, (cpu->PC+1) & 0xff);
+    operandB = cpu_ram_read(cpu, (cpu->PC+2) & 0xff);
+    
+    int add_to_pc = (IR >> 6) + 1;
+
+    // printing it out: 02X =lead with zeros, 2 digits wide, x means capital lettering
+    printf("TRACE:  %02X: %02X %02X %02X\n", cpu->PC, IR, operandA, operandB);
+
+    switch(IR) {
+      //
+      case LDI:
+        // register number 0, set to the value 8
+        cpu->reg[operandA] = operandB;
+        // moves the PC to the next instruction
+        // cpu->PC += 3;
+        break;
+
+      // PRN- print out the numeric value in a register
+      case PRN:
+        printf("%d\n", cpu->reg[operandA]);
+        // moves the PC to the next instruction
+        // cpu->PC += 2;
+        break;
+
+      // Halt
+      case HLT:
+        // if running is false then break
+        running = 0;
+        // moves the PC to the next instruction
+        // cpu->PC += 1;
+        break;
+
+    }
+    // moves the PC to the next instruction
+    cpu->PC += add_to_pc;
     // 5. Do whatever the instruction should do according to the spec.
     // 6. Move the PC to the next instruction.
   }
@@ -64,4 +119,9 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
+  // start at address 0
+  cpu->PC = 0;
+  // zero registers and RAM, memeset = is used to fill a block of memory w/ a particular value: GeeksforGeeks
+  memset(cpu->ram, 0, sizeof cpu->ram);
+  memset(cpu->ram, 0, sizeof cpu->reg);
 }
