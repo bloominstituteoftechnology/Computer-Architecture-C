f0b2cfb8ef5056a8a840c003f092f6a1d9699819
lajawanti <myemail>
2019-02-25 18:44:29 -0500

    working on cpu-run()

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -61,17 +61,30 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
  */
 void cpu_run(struct cpu *cpu)
 {
-  int running = 1; // True until we get a HLT instruction
+    int running = 1; // True until we get a HLT instruction
 
-  while (running) {
-    // TODO
-    // 1. Get the value of the current instruction (in address PC).
-    // 2. Figure out how many operands this next instruction requires
-    // 3. Get the appropriate value(s) of the operands following this instruction
-    // 4. switch() over it to decide on a course of action.
-    // 5. Do whatever the instruction should do according to the spec.
-    // 6. Move the PC to the next instruction.
-  }
+    while (running) {
+        // TODO
+        // 1. Get the value of the current instruction (in address PC).
+        unsigned char IR = cpu_ram_read(cpu, cpu->PC); //Instruction Register
+        
+        // 2. Figure out how many operands this next instruction requires
+        // 3. Get the appropriate value(s) of the operands following this instruction
+        unsigned char operandA = cpu_ram_read(cpu, (cpu->PC + 1));
+        unsigned char operandB = cpu_ram_read(cpu, (cpu->PC + 2));
+        
+        // 4. switch() over it to decide on a course of action.
+        switch (IR)
+        {
+            case 'ADD':
+              break;
+          
+            default:
+              break;
+        }
+        // 5. Do whatever the instruction should do according to the spec.
+        // 6. Move the PC to the next instruction.
+    }
 }
 
 /**
