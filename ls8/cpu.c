9be114e4e4609821c3ae8abeb385415caf69c176
stdemicheli <stdemicheli@gmail.com>
2019-02-25 22:15:43 +0100

    Initial commit.

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -9,7 +9,7 @@ void cpu_load(struct cpu *cpu)
 {
   char data[DATA_LEN] = {
     // From print8.ls8
-    0b10000010, // LDI R0,8
+    0b10000010, // LDI R0, 8
     0b00000000,
     0b00001000,
     0b01000111, // PRN R0
