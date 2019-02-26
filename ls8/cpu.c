ff1ebdad7697ff5a07779aac7b7e1907c827f32e
Chiboba <briangfelix@gmail.com>
2019-02-25 17:34:11 -0800

    LDI, HLT, PRN functions

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -61,9 +61,18 @@ void cpu_run(struct cpu *cpu)
     unsigned char IR = cpu_ram_read(cpu, cpu->PC);
     unsigned char operandA = cpu_ram_read(cpu, cpu->PC + 1);
     unsigned char operandB = cpu_ram_read(cpu, cpu->PC + 2);
-    printf("%d IR\n", IR );
-    printf("%d oA\n", operandA);
-    printf("%d oB\n", operandB);
+    unsigned int numop = IR >> 6;
+    switch(IR) {
+      case(LDI):
+        cpu->registers[operandA] = operandB;
+        printf("%i INSIDE LDI\n", cpu->registers[operandA]);
+      case(PRN):
+        printf("Numeric Value = %i\n", cpu->registers[operandA]);
+      case(HLT):
+        running = 0;
+    }
+    cpu->PC += numop + 1;
+    
     
     // 2. Figure out how many operands this next instruction requires
     // 3. Get the appropriate value(s) of the operands following this instruction
