d47db3a5527db504e92a372296f576704a9be844
jsmnglng <jsmn.glng@gmail.com>
2019-02-26 12:39:36 -0800

    Implement cpu_run() and added LDI, PRN, HTL instructions

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -66,16 +66,45 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
+  unsigned char IR, operand0, operand1;
 
   while (running)
   {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
+    IR = cpu_ram_read(cpu, cpu->PC);
+
     // 2. Figure out how many operands this next instruction requires
     // 3. Get the appropriate value(s) of the operands following this instruction
+    operand0 = cpu_ram_read(cpu, cpu->PC + 1);
+    operand1 = cpu_ram_read(cpu, cpu->PC + 2);
+
     // 4. switch() over it to decide on a course of action.
     // 5. Do whatever the instruction should do according to the spec.
     // 6. Move the PC to the next instruction.
+    switch (IR)
+    {
+    case LDI:
+      // sets value of operand1 to the reg[operand0]
+      cpu->reg[operand0] = operand1;
+      // Move the PC to the next instruction.
+      cpu->PC += 3;
+      break;
+    case PRN:
+      printf("%d\n", cpu->reg[operand0]);
+      // Move the PC to the next instruction.
+      cpu->PC += 2;
+      break;
+    case HLT:
+      // Set running to false to stop program
+      running = 0;
+      // Move the PC to the next instruction.
+      cpu->PC += 1;
+      break;
+    default:
+      printf("Unexpected instruction 0x%02X at 0x%02X\n", IR, cpu->PC);
+      exit(1);
+    }
   }
 }
 
