07655f880517c34ae905214ab12e8d6aa01dac25
Austinlp4 <apendergrast44@gmail.com>
2019-02-25 18:46:23 -0600

    progress on CPU run

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,6 +1,8 @@
 #include "cpu.h"
-
+#define ADDR_EMPTY_STACK 0XF4
 #define DATA_LEN 6
+#include <string.h>
+#include <stdio.h>
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
@@ -8,6 +10,11 @@
 void cpu_ram_write(struct cpu *cpu, unsigned char value, unsignded char address)
 {
   cpu->ram[address] = value;
+}
+
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address)
+{
+  return cpu->ram[address];
 }
 
 void cpu_load(struct cpu *cpu)
@@ -52,11 +59,26 @@ void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
+  // initialize operands
+  unsigned char operandA = NULL;
+  unsigned char operandB = NULL;
+
   while (running) {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
+    unsigned char instruction = cpu_ram_read(cpu, cpu->PC);
     // 2. Figure out how many operands this next instruction requires
+    unsigned int operands = instruction >> 6; 
     // 3. Get the appropriate value(s) of the operands following this instruction
+    if(operands == 2)
+    {
+      operandA = cpu_ram_read(cpu, (cpu->PC+1) & 0xff);
+      operandB = cpu_ram_read(cpu, (cpu->PC+2) & 0xff);
+    }
+    else if (operands == 1)
+    {
+      operandA = cpu_ram_read(cpu, (cpu->PC+1) & 0xff);
+    }
     // 4. switch() over it to decide on a course of action.
     // 5. Do whatever the instruction should do according to the spec.
     // 6. Move the PC to the next instruction.
@@ -69,4 +91,9 @@ void cpu_run(struct cpu *cpu)
 void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
+  cpu->PC = -1;
+  memset(cpu->ram, 0, sizeof cpu->ram);
+  memset(cpu->reg, 0, sizeof cpu->reg);
+  cpu->SP = ADDR_EMPTY_STACK;
+  cpu->E = 0;
 }
