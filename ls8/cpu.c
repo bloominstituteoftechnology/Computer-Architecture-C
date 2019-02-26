21d179b5754870bcb91b1c0634ac4915dd53c1ea
Conner Alegre <cga0@pm.me>
2019-02-25 17:34:26 -0700

    cpu_run with HLT, LDI, and PRN instruction execution

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -8,18 +8,19 @@
 void cpu_load(struct cpu *cpu)
 {
   char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
+      // From print8.ls8
+      0b10000010, // LDI R0,8
+      0b00000000,
+      0b00001000,
+      0b01000111, // PRN R0
+      0b00000000,
+      0b00000001 // HLT
   };
 
   int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
+  for (int i = 0; i < DATA_LEN; i++)
+  {
     cpu->ram[address++] = data[i];
   }
 
@@ -31,42 +32,14 @@ void cpu_load(struct cpu *cpu)
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
-  switch (op) {
-    case ALU_MUL:
-      // TODO
-      break;
+  switch (op)
+  {
+  case ALU_MUL:
+    // TODO
+    break;
 
     // TODO: implement more ALU ops
   }
-}
-
-/**
- * Run the CPU
- */
-void cpu_run(struct cpu *cpu)
-{
-  int running = 1; // True until we get a HLT instruction
-
-  while (running) {
-    // TODO
-    // 1. Get the value of the current instruction (in address PC).
-    // 2. Figure out how many operands this next instruction requires
-    // 3. Get the appropriate value(s) of the operands following this instruction
-    // 4. switch() over it to decide on a course of action.
-    // 5. Do whatever the instruction should do according to the spec.
-    // 6. Move the PC to the next instruction.
-  }
-}
-
-/**
- * Initialize a CPU struct
- */
-void cpu_init(struct cpu *cpu)
-{
-  // TODO: Initialize the PC and other special registers
-  cpu->pc = 0;
-  memset(cpu->ram, 0, 256 * sizeof(unsigned char));
-  memset(cpu->reg, 0, 8 * sizeof(unsigned char));
 }
 
 /**
@@ -83,4 +56,62 @@ char cpu_ram_read(struct cpu *cpu, unsigned char address)
 void cpu_ram_write(struct cpu *cpu, unsigned char value, unsigned char address)
 {
   cpu->ram[address] = value;
+}
+
+/**
+ * Perform LDI Instruction
+ */
+void ldi_instr(struct cpu *cpu)
+{
+  unsigned char operand_a = cpu_ram_read(cpu, cpu->pc + 1);
+  unsigned char operand_b = cpu_ram_read(cpu, cpu->pc + 2);
+  cpu->reg[operand_a] = operand_b;
+  cpu->pc += 3;
+}
+
+/**
+ * Perform PRN Instruction
+ */
+void prn_instr(struct cpu *cpu)
+{
+  unsigned char operand_a = cpu_ram_read(cpu, cpu->pc + 1);
+  unsigned char print_value = cpu->reg[operand_a];
+  
+  printf("%u \n", print_value);
+
+  cpu->pc += 2;
+}
+
+/**
+ * Run the CPU
+ */
+void cpu_run(struct cpu *cpu)
+{
+  int running = 1; // True until we get a HLT instruction
+
+  while (running)
+  {
+    unsigned char c_instr = cpu_ram_read(cpu, cpu->pc);
+    switch (c_instr)
+    {
+    case LDI:
+      ldi_instr(cpu);
+      break;
+    case PRN:
+      prn_instr(cpu);
+      break;
+    case HLT:
+      running = 0;
+    }
+  }
+}
+
+/**
+ * Initialize a CPU struct
+ */
+void cpu_init(struct cpu *cpu)
+{
+  cpu->pc = 0;
+  memset(cpu->ram, 0, 256 * sizeof(unsigned char));
+  memset(cpu->reg, 0, 8 * sizeof(unsigned char));
 }
