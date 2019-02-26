a5111cffd1d330461a2caacc316b166505b66863
ezra davis davis <ezra.rr.davis@gmail.com>
2019-02-26 13:56:30 -0800

    almsot done

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,5 +1,7 @@
 #include "cpu.h"
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define DATA_LEN 6
 
@@ -23,23 +25,61 @@ unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address){
 }
 
 
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char filename[])
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
-
-  int address = 0;
-
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  FILE *fptr;
+  char *ptr;
+  char buffer[255];
+  char cmdString[9];
+  
+  unsigned int address = 0;
+  
+  fptr = fopen(filename ? filename : "examples/mult.ls8", "r");
+  if(fptr){
+   // printf("%d\n", fptr[0]);
+   while(fgets(buffer,255,fptr)){
+    //printf("%s\n", buffer);
+    //printf("%s\n",strncpy(data,buffer,9));
+    if(buffer){
+      strncpy(cmdString,buffer,8);
+      //printf("%s\n", cmdString);
+      unsigned char cmd = strtoul(cmdString, &ptr ,2);
+      // printf("%d\n", cmd);
+      cpu->ram[address++] = cmd;    
+    }        
   }
+  } else {
+    printf("Null");
+  }
+  
+  // printf("\n%d\n", cpu->ram[0]);
+  // printf("%d\n", cpu->ram[1]);
+  // printf("%d\n", cpu->ram[2]);
+  // printf("%d\n", cpu->ram[3]);
+
+  
+  
+
+
+  // char data[DATA_LEN] = {
+  //   // From print8.ls8
+  //   0b10000010, // LDI R0,8
+  //   0b00000000,
+  //   0b00001000,
+  //   0b01000111, // PRN R0
+  //   0b00000000,
+  //   0b00000001  // HLT
+  // };
+
+
+  
+  // //printf("%s",data);
+
+  // int address = 0;
+
+  // for (int i = 0; i < DATA_LEN; i++) {
+  //   cpu->ram[address++] = data[i];
+  // }
 
   // TODO: Replace this with something less hard-coded
 }
@@ -64,25 +104,41 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
-
   while (running) {
+    // printf("\n%d",cpu->pc);
     unsigned char instruction = cpu_ram_read(cpu, cpu->pc);
     unsigned int operand = instruction >> 6;
-    //printf("\n%d",operand);
+    //printf("%d\n",instruction);
 
     unsigned char operandA =  cpu_ram_read(cpu, cpu->pc+1);
     unsigned char operandB =  cpu_ram_read(cpu, cpu->pc+2);
+    
     switch (instruction)
     {
       case HLT:
         running = 0;
         break;
       case LDI:
+        //printf("hi");
+        //printf("%d",cpu->pc);
         cpu->registry[operandA] = operandB;
         break;
       case PRN:
-        printf("\n%d", cpu->registry[operandA]);
+        printf("%d\n", cpu->registry[operandA]);
         break;
+      case MUL:
+        cpu->registry[operandA] *= cpu->registry[operandB];
+        break;
+      case PUSH:
+        cpu->SP -= 1;
+        cpu->ram[cpu->SP] = cpu->registry[operandA];
+        break;
+      case POP:
+        cpu->ram[cpu->SP] = cpu->registry[operandA];
+        cpu->SP += 1;        
+        break;
+
+
     
       default:
         break;
@@ -106,6 +162,7 @@ void cpu_init(struct cpu *cpu)
 {
   // TODO: Initialize the PC and other special registers
   memset(cpu->ram, 0, sizeof cpu->ram);
-  memset(cpu->registry,0,sizeof cpu->ram);
+  memset(cpu->registry,0,sizeof cpu->registry);
   cpu->pc = 0;
+  cpu->SP = 0xf4;
 }
--- ls8/cpu.h
+++ ls8/cpu.h
@@ -7,6 +7,7 @@ struct cpu
   unsigned int pc;
   unsigned char registry[8];
   unsigned char ram[256];
+  unsigned char SP;
 };
 
 // ALU operations
@@ -23,11 +24,14 @@ enum alu_op {
 #define LDI  0b10000010
 #define HLT  0b00000001
 #define PRN  0b01000111
+#define MUL  0B10100010
+#define PUSH 0B01000101
+#define POP  0B01000110
 // TODO: more instructions here. These can be used in cpu_run().
 
 // Function declarations
 
-extern void cpu_load(struct cpu *cpu);
+extern void cpu_load(struct cpu *cpu, char *filename);
 extern void cpu_init(struct cpu *cpu);
 extern void cpu_run(struct cpu *cpu);
 
