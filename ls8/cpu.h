b062082248d54e37e5601f0fe44ec4b7fb33f5af
lndubose <lndubose13@gmail.com>
2019-02-27 17:21:36 -0600

    * Implement the CALL and RET instructions and Implement Subroutine Calls

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -16,8 +16,9 @@ struct cpu
 // ALU operations
 enum alu_op
 {
-  ALU_MUL
+  ALU_MUL,
   // Add more here
+  ALU_ADD
 };
 
 // Instructions
@@ -29,8 +30,11 @@ enum alu_op
 #define HLT 0b00000001
 #define PRN 0b01000111
 #define MUL 0b10100010
+#define ADD 0b10100000
 #define POP 0b01000110
 #define PUSH 0b01000101
+#define CALL 0b01010000
+#define RET 0b00010001
 
 // TODO: more instructions here. These can be used in cpu_run().
 
