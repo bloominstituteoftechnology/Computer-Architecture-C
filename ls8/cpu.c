4cb11e771b04cb7f25059d0cc576d9b043c0fc7e
lndubose <lndubose13@gmail.com>
2019-02-26 18:23:25 -0600

    Beautify cpu_run() loop

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define DATA_LEN 6
+// ================== RAM functions ===================
 
 /**  Read from the CPU's ram at the specifc location 
  */
@@ -18,6 +18,31 @@ void cpu_ram_write(struct cpu *cpu, unsigned char MAR, unsigned char num)
 {
   cpu->ram[MAR] = num;
 }
+
+// ================== Instrcution Handlers ===================
+
+// LDI function: Set the value of a register to an integer
+void handle_LDI(struct cpu *cpu)
+{
+  unsigned char regA = cpu_ram_read(cpu, (cpu->pc + 1));
+  unsigned char value = cpu_ram_read(cpu, (cpu->pc + 2));
+  cpu->registers[regA] = value;
+};
+
+// PRN function: Print numeric value stored in the given register
+void handle_PRN(struct cpu *cpu)
+{
+  unsigned char regA = cpu_ram_read(cpu, (cpu->pc + 1));
+  printf("%d\n", cpu->registers[regA]);
+}
+
+// HLT function: Halt the CPU (and exit the emulator)
+void handle_HLT()
+{
+  exit(0);
+}
+
+// ================== CPU functions ===================
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
@@ -34,29 +59,11 @@ void cpu_load(struct cpu *cpu, char *argv)
     if (c[0] == '1' || c[0] == '0')
     {
       ret = strtoul(c, NULL, 2);
-      cpu->ram[address++] = ret;
+      cpu_ram_write(cpu, address++, ret);
     }
   }
 
   fclose(ptr);
-
-  // Pevious hardcode
-  // char data[DATA_LEN] = {
-  //     // From print8.ls8
-  //     0b10000010, // LDI R0,8
-  //     0b00000000,
-  //     0b00001000,
-  //     0b01000111, // PRN R0
-  //     0b00000000,
-  //     0b00000001 // HLT
-  // };
-
-  // int address = 0;
-
-  // for (int i = 0; i < DATA_LEN; i++)
-  // {
-  //   cpu->ram[address++] = data[i];
-  // }
 
   // TODO: Replace this with something less hard-coded
 }
@@ -64,11 +71,11 @@ void cpu_load(struct cpu *cpu, char *argv)
 /**
  * ALU
  */
-void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
+void alu(struct cpu *cpu, enum alu_op op)
 {
-  (void)cpu;
-  (void)regA;
-  (void)regB;
+  unsigned char regA = cpu_ram_read(cpu, (cpu->pc + 1));
+  unsigned char regB = cpu_ram_read(cpu, (cpu->pc + 2));
+
   switch (op)
   {
   case ALU_MUL:
@@ -98,23 +105,21 @@ void cpu_run(struct cpu *cpu)
     // 5. Do whatever the instruction should do according to the spec.
     // 6. Move the PC to the next instruction.
     unsigned char IR = cpu_ram_read(cpu, (cpu->pc));
-    unsigned char operandA = cpu_ram_read(cpu, (cpu->pc + 1));
-    unsigned char operandB = cpu_ram_read(cpu, (cpu->pc + 2));
 
     switch (IR)
     {
     case LDI:
-      cpu->registers[operandA] = operandB;
+      handle_LDI(cpu);
       break;
     case PRN:
-      printf("%d\n", cpu->registers[operandA]);
+      handle_PRN(cpu);
       break;
     case HLT:
-      running = 0;
+      handle_HLT();
       break;
 
     case MUL:
-      alu(cpu, ALU_MUL, operandA, operandB);
+      alu(cpu, ALU_MUL);
       break;
 
     default:
