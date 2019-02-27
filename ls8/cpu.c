827a68ffed7df05011fb5049f5b86dc35da3886a
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-02-26 19:13:16 -0700

    Refactor cpu_run to use a Jump Table.

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -49,46 +49,11 @@ void cpu_load(struct cpu *cpu, char *path)
 
   fclose(src);
 
-  // for (char c = getc(src); c != EOF; c = getc(src)) {
-  //   int wait = 0;
-  //   int line = 0;
-  //   char word[8];
-  //   char *meh;
-
-  // if (wait >= 0) {
-  //     if (c == '0' || c == '1') {
-  //       word[wait] = c;
-  //       wait += 1;
-  //       if (wait == 8) {
-  //         wait = -1;
-  //         data2[line] = strtoul(word, meh, 2);
-  //         line += 1;
-  //       }
-  //     } else {
-  //       wait = -1;
-  //     }
-
-  // } else if (c == '\n') {
-  //   wait = 0;
-  // }
-  // }
-
-  // char data[DATA_LEN] = {
-  //   // From print8.ls8
-  //   0b10000010, // LDI R0,8
-  //   0b00000000,
-  //   0b00001000,
-  //   0b01000111, // PRN R0
-  //   0b00000000,
-  //   0b00000001  // HLT
-  // };
-
   int address = 0;
 
   for (int i = 0; i < count + 1; i++) {
     cpu->ram[address++] = data[i];
   }
-  // TODO: Replace this with something less hard-coded
 }
 
 unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address) {
@@ -113,15 +78,24 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
   }
 }
 
+
 /**
- * Run the CPU
+ * Handler Functions
  */
+
+void (*handlers[256])(struct cpu *cpu, unsigned char op0, unsigned char op1) = {0};
+
+void LDI_handler (struct cpu *cpu, unsigned char op0, unsigned char op1) { cpu->registers[op0] = op1; } // Set Register  
+void PRN_handler (struct cpu *cpu, unsigned char op0, unsigned char op1) { (void)op1; printf("%d\n", cpu->registers[op0]); } // Print
+void MUL_handler (struct cpu *cpu, unsigned char op0, unsigned char op1) { alu(cpu, ALU_MUL, op0, op1); } // Multiply
+void HLT_handler (struct cpu *cpu, unsigned char op0, unsigned char op1) { (void)cpu; (void)op0; (void)op1; exit(0); } // Halt
+
 void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
   while (running) {
-    // TODO
+
     // 1. Get the value of the current instruction (in address PC).
     unsigned char IR = cpu_ram_read(cpu, cpu->PC);
 
@@ -133,30 +107,14 @@ void cpu_run(struct cpu *cpu)
     unsigned char op1 = cpu_ram_read(cpu, cpu->PC + 2);
 
     // 4. switch() over it to decide on a course of action.
-    switch(IR) {
+    handlers[LDI] = LDI_handler;
+    handlers[PRN] = PRN_handler;
+    handlers[MUL] = MUL_handler;
+    handlers[HLT] = HLT_handler;
 
     // 5. Do whatever the instruction should do according to the spec.
-      case LDI:
-      cpu->registers[op0] = op1;
-      break;
-
-      case PRN:
-      printf("%d\n", cpu->registers[op0]);
-      break;
-
-      case MUL:
-      alu(cpu, ALU_MUL, op0, op1);
-      break;
-      
-      case HLT:
-      running = 0;
-      break;
-      
-      default:
-      printf("Unkown Instruction: 0x%02x\n", IR);
-      running = 0;
-      break;
-    }
+    if (handlers[IR]) { handlers[IR](cpu, op0, op1); }
+    else { printf("Unkown Instruction: 0x%02x\n", IR); running = 0;}
 
     // 6. Move the PC to the next instruction.
     cpu->PC += (ops + 1);
