fc04d1c8cf7c4150af03cda5724606bcc09cfdca
Kevin Brack <brackkevin@gmail.com>
2019-02-27 14:54:57 -0600

    Un-hard-codes program input file

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -8,26 +8,17 @@
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu, char **filename)
+void cpu_load(struct cpu *cpu, char *filename)
 {
-  // char data[DATA_LEN] = {
-  //     // From print8.ls8
-  //     0b10000010, // LDI R0,8
-  //     0b00000000,
-  //     0b00001000,
-  //     0b01000111, // PRN R0
-  //     0b00000000,
-  //     0b00000001 // HLT
-  // };
-
-  // int address = 0;
-
-  // for (int i = 0; i < DATA_LEN; i++)
-  // {
-  //   cpu->ram[address++] = data[i];
-  // }
-
-  // TODO: Replace this with something less hard-coded
+  FILE *fp = fopen(filename, "r");
+  char line[8192];
+  int address = 0;
+  while (fgets(line, sizeof(line), fp) != NULL)
+  {
+    unsigned char val = strtoul(line, NULL, 2);
+    cpu_ram_write(cpu, address++, val);
+  }
+  fclose(fp);
 }
 
 /**
@@ -271,6 +262,8 @@ void cpu_run(struct cpu *cpu)
 
     /* you can have any number of case statements */
     default: /* Optional */
+      printf("unknown command $u\n", _IR);
+      exit(1);
       break;
     }
     // 5. Do whatever the instruction should do according to the spec.
