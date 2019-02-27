0c3beb8445044b95ded6c8c8aaff0c1b90cb8b2e
Eva <evalee904@gmail.com>
2019-02-27 15:57:26 -0600

    load_cpu()

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,6 +1,7 @@
 #include "cpu.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define DATA_LEN 6
 
@@ -17,26 +18,50 @@ void cpu_ram_write(struct cpu *cpu, unsigned char mar, unsigned char mdr)
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char *filename)
 {
-  char data[DATA_LEN] = {
-      // From print8.ls8
-      0b10000010, // LDI R0,8
-      0b00000000,
-      0b00001000,
-      0b01000111, // PRN R0
-      0b00000000,
-      0b00000001 // HLT
-  };
+  // char data[DATA_LEN] = {
+  //     // From print8.ls8
+  //     0b10000010, // LDI R0,8
+  //     0b00000000,
+  //     0b00001000,
+  //     0b01000111, // PRN R0
+  //     0b00000000,
+  //     0b00000001 // HLT
+  // };
 
-  int address = 0;
+  // int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++)
-  {
-    cpu->ram[address++] = data[i];
-  }
+  // for (int i = 0; i < DATA_LEN; i++)
+  // {
+  //   cpu->ram[address++] = data[i];
+  // }
 
   // TODO: Replace this with something less hard-coded
+  FILE *fp = fopen(filename, "r");
+  char line[1024];
+  // RAM address
+  unsigned char address = 0x00;
+  // open the source file
+  if (fp == NULL)
+  {
+    fprintf(stderr, "\nError opening file.\n");
+    exit(1);
+  }
+
+  while (fgets(line, sizeof(line), fp) != NULL)
+  {
+    // convert binary to number
+    char *ptr;
+    unsigned char ret = strtoul(line, &ptr, 2);
+
+    if (ptr == line)
+    {
+      continue;
+    }
+    cpu_ram_write(cpu, ret, address++);
+  }
+  fclose(fp);
 }
 
 /**
