581c2cb48f8a5d7ce51222d9d20a3151e5c346ea
Michael Agard <mdagard@gmail.com>
2019-03-04 17:54:46 -0500

    fill in struct cpu

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -4,9 +4,12 @@
 // Holds all information about the CPU
 struct cpu {
   // TODO
-  // PC
+  // PC: Program Counter, address of the currently executing instruction
+  unsigned int PC;
   // registers (array)
+  unsigned char registers[8];
   // ram (array)
+  unsigned char ram[256];
 };
 
 // ALU operations
