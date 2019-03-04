3eb21252f09ec137fd03419d9050f7af35f388fd
AM_Myrick <amyrick.820@gmail.com>
2019-03-04 16:11:32 -0500

    adds ram functions

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,6 +1,16 @@
 #include "cpu.h"
 
 #define DATA_LEN 6
+
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address)
+{
+    return cpu->ram[address];
+}
+
+void cpu_ram_write(struct cpu *cpu, unsigned char address, unsigned char value)
+{
+    cpu->ram[address] = value;
+}
  
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
