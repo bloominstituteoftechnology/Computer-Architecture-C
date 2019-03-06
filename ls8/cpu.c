29785d59b9ec65a5dd44a5b71478db1f1f63c69a
luiscmartinez <mrluismartinezzz@gmail.com>
2019-03-06 15:45:35 -0800

    pushing old & new content

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,29 +1,56 @@
 #include "cpu.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define DATA_LEN 6
+
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char MAR)
+{
+  return cpu->ram[MAR];
+}
+
+void cpu_ram_write(struct cpu *cpu, unsigned char MAR, unsigned char MDR)
+{
+  cpu->ram[MAR] = MDR;
+}
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char *filename)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+  //Init the file
+  FILE *fp = fopen(filename, "r");
 
-  int address = 0;
+  char line[1024];
 
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
+  unsigned char mar = 0x00;
+
+  //Check if the file exits:
+  if (fp == NULL)
+  {
+    fprintf(stderr, "error opening file %s\n", filename);
+    exit(1);
   }
 
-  // TODO: Replace this with something less hard-coded
+  //While there's lines in the file...
+  while (fgets(line, sizeof line, fp) != NULL)
+  {
+    char *endptr;
+    unsigned char machine_code = strtoul(line, &endptr, 2);
+
+    if (endptr == line)
+    {
+      //we got no numbers
+      continue;
+    }
+
+    //Write it into Memory
+    cpu_ram_write(cpu, mar++, machine_code);
+  }
+
+  fclose(fp);
 }
 
 /**
@@ -31,11 +58,15 @@ void cpu_load(struct cpu *cpu)
  */
 void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB)
 {
-  switch (op) {
-    case ALU_MUL:
-      // TODO
-      break;
+  switch (op)
+  {
+  case ALU_MUL:
+    cpu->reg[regA] *= cpu->reg[regB];
+    break;
 
+  case ALU_ADD:
+    cpu->reg[regA] += cpu->reg[regB];
+    break;
     // TODO: implement more ALU ops
   }
 }
@@ -45,16 +76,83 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
  */
 void cpu_run(struct cpu *cpu)
 {
-  int running = 1; // True until we get a HLT instruction
+  int running = 1;
 
-  while (running) {
-    // TODO
-    // 1. Get the value of the current instruction (in address PC).
-    // 2. Figure out how many operands this next instruction requires
-    // 3. Get the appropriate value(s) of the operands following this instruction
-    // 4. switch() over it to decide on a course of action.
-    // 5. Do whatever the instruction should do according to the spec.
-    // 6. Move the PC to the next instruction.
+  unsigned char IR;
+
+  while (running)
+  {
+    IR = cpu_ram_read(cpu, cpu->PC);
+    unsigned char operandA = cpu_ram_read(cpu, (cpu->PC + 1) & 0xFF);
+    unsigned char operandB = cpu_ram_read(cpu, (cpu->PC + 2) & 0xFF);
+
+    int add_to_pc = (IR >> 6) + 1;
+    switch (IR)
+    {
+    case LDI:
+      cpu->reg[operandA] = operandB;
+      break;
+    case MUL:
+      alu(cpu, ALU_MUL, operandA, operandB);
+      break;
+
+    case ADD:
+      alu(cpu, ALU_ADD, operandA, operandB);
+      break;
+
+    case PUSH:
+      cpu_ram_write(cpu, --cpu->reg[7], cpu->reg[operandA]);
+      break;
+
+    case POP:
+      cpu->reg[operandA] = cpu_ram_read(cpu, cpu->reg[7]++);
+      break;
+
+    case CALL:
+      cpu_ram_write(cpu, --cpu->reg[7], ++cpu->PC);
+      cpu->PC = cpu->reg[operandA];
+      add_to_pc = 0;
+      break;
+    case RET:
+      cpu->PC = cpu_ram_read(cpu, cpu->reg[7]++);
+      break;
+    case CMP:
+      if (cpu->reg[operandA] == cpu->reg[operandB])
+      {
+        cpu->flag_reg[0] = 1;
+      }
+      else if (cpu->reg[operandA] > cpu->reg[operandB])
+      {
+        cpu->flag_reg[1] = 1;
+      }
+      else
+      {
+        cpu->flag_reg[2] = 1;
+      }
+      break;
+    case JMP:
+      cpu->PC = cpu->reg[operandA];
+      add_to_pc = 0;
+      break;
+    case JEQ:
+      if (cpu->flag_reg[0] == 1)
+      {
+        cpu->PC = cpu->reg[operandA];
+        add_to_pc = 0;
+      }
+      break;
+    case JNE:
+      if (cpu->flag_reg[0] == 0)
+      {
+        cpu->PC = cpu->reg[operandA];
+        add_to_pc = 0;
+      }
+      break;
+    case HLT:
+      running = 0;
+      break;
+    }
+    cpu->PC += add_to_pc;
   }
 }
 
@@ -63,5 +161,11 @@ void cpu_run(struct cpu *cpu)
  */
 void cpu_init(struct cpu *cpu)
 {
-  // TODO: Initialize the PC and other special registers
+  cpu->PC = 0;
+
+  memset(cpu->ram, 0, sizeof(cpu->ram));
+  memset(cpu->reg, 0, sizeof(cpu->reg));
+  memset(cpu->flag_reg, 0, sizeof(cpu->flag_reg));
+
+  cpu->reg[7] = 0b11110100;
 }
