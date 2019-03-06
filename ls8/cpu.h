54b20f97135cc569e3b28bd4a206e57365f2cf51
Michael Agard <mdagard@gmail.com>
2019-03-06 15:55:00 -0500

    add ALU_ADD and define literals, mod cpu_load to accpet file

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -17,7 +17,8 @@ struct cpu {
 
 // ALU operations
 enum alu_op {
-	ALU_MUL
+	ALU_MUL,
+  ALU_ADD
 	// Add more here
 };
 
@@ -25,16 +26,22 @@ enum alu_op {
 
 // These use binary literals. If these aren't available with your compiler, hex
 // literals should be used.
-
+// where programs start getting loaded
+#define ADDR_PROGRAM_ENTRY 0x00
+// where sp is on an empty stack
+#define ADDR_EMPTY_STACK 0xF4
+// these use binary literals.
 #define LDI  0b10000010
 #define HLT  0b00000001
 #define PRN  0b01000111
+#define MUL  0b10100010
+
 // TODO: more instructions here. These can be used in cpu_run().
 
 // Function declarations
 
-extern void cpu_load(struct cpu *cpu);
 extern void cpu_init(struct cpu *cpu);
+extern void cpu_load(char *filename, struct cpu *cpu);
 extern void cpu_run(struct cpu *cpu);
 
 #endif
