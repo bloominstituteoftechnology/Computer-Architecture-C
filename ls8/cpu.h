01204b1d10c45144971272f787761f8cc110c4e6
Michael Agard <mdagard@gmail.com>
2019-03-08 09:28:42 -0500

    add ADD, POP, PUSH, CALL

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -34,7 +34,11 @@ enum alu_op {
 #define LDI  0b10000010
 #define HLT  0b00000001
 #define PRN  0b01000111
+#define ADD  0b10100000
 #define MUL  0b10100010
+#define POP  0b01000110
+#define PUSH 0b01000101
+#define CALL 0b01010000
 
 // TODO: more instructions here. These can be used in cpu_run().
 
