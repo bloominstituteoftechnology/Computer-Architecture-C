c763262a194781f6f243229646dcf304a642881d
qtn2932 <quan.nguyen199712@gmail.com>
2019-04-01 18:31:53 -0400

    day one mvp

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -1,15 +1,16 @@
+    
 #include <stdio.h>
 #include "cpu.h"
 
 /**
  * Main
  */
-int main(void)
+int main(int argc, char *argv[])
 {
   struct cpu cpu;
 
   cpu_init(&cpu);
-  cpu_load(&cpu);
+  cpu_load(&cpu, argc, argv);
   cpu_run(&cpu);
 
   return 0;
