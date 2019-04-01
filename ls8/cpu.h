2d1d2775ed4f5441326208f9e4c3a3890a826264
adamhinckley <adamhinckley@mac.com>
2019-04-01 14:37:11 -0600

    Step 1 done

--- ls8/cpu.h
+++ ls8/cpu.h
@@ -2,17 +2,22 @@
 #define _CPU_H_
 
 // Holds all information about the CPU
-struct cpu {
+struct cpu
+{
   // TODO
   // PC
+  unsigned char PC;
   // registers (array)
+  unsigned char registers[8];
   // ram (array)
+  unsigned char ram[256];
 };
 
 // ALU operations
-enum alu_op {
-	ALU_MUL
-	// Add more here
+enum alu_op
+{
+  ALU_MUL
+  // Add more here
 };
 
 // Instructions
@@ -20,9 +25,9 @@ enum alu_op {
 // These use binary literals. If these aren't available with your compiler, hex
 // literals should be used.
 
-#define LDI  0b10000010
-#define HLT  0b00000001
-#define PRN  0b01000111
+#define LDI 0b10000010
+#define HLT 0b00000001
+#define PRN 0b01000111
 // TODO: more instructions here. These can be used in cpu_run().
 
 // Function declarations
