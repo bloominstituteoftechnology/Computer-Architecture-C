6c646d59bf0bdd1c5e2ce70c14703f2e737bd3cd
Clantigua2 <clantigua954@gmail.com>
2019-04-01 16:24:53 -0400

    adding notes and changing some stuff around

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,6 +1,11 @@
 #include "cpu.h"
 
 #define DATA_LEN 6
+#define LDI 0b10000010
+#define NOP 0b00000000
+#define PRN 0b01000111
+#define HLT 0b00000001
+#define STR 0b00001000 // store the next value to reg
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
@@ -9,17 +14,24 @@ void cpu_load(struct cpu *cpu)
 {
   char data[DATA_LEN] = {
     // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
+    //LDI load immediate, store a value in reg or set this reg to this value
+    LDI, // LDI R0,8 
+    NOP,
+    STR, //README says this is value of 8
+    8, // make STR store this to registry
+    //PRN prints the numberic value stored in a reg
+    PRN, // PRN R0
+    NOP,
+    // halt the cpu and exit the emulator
+    HLT  // HLT
   };
 
   int address = 0;
 
   for (int i = 0; i < DATA_LEN; i++) {
+    if (data[i] == STR) {
+      cpu->registers[address] = data[i + 1];
+    }
     cpu->ram[address++] = data[i];
   }
 
