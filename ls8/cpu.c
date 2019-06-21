f147788e9061ee67cd353fde6c15cbacb692222d
Sergey Osipyan <osipyansergey@gmail.com>
2019-06-21 13:29:45 -0700

    MVP

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -16,7 +16,7 @@ void cpu_load(struct cpu *cpu, char *file_name) {
   fp = fopen(file_name, "r");
   if (fp == NULL) { 
     fprintf(stderr, "Error - Opening File Failed\n"); 
-    exit (1); 
+    exit(1); 
   }
   while (fgets(line, 128, fp) != NULL) {
     char *endptr;
@@ -45,10 +45,13 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
   switch (op) {
     case ALU_MUL:
       // TODO
-      cpu->reg[regA] = cpu->reg[regA] * cpu->reg[regB];
+      cpu->reg[regA] *= cpu->reg[regB];
       break;
 
     // TODO: implement more ALU ops
+    case ALU_ADD:
+      cpu->reg[regA] += cpu->reg[regB];
+      break;
   }
 }
 unsigned char cpu_ram_read(struct cpu *cpu, unsigned char index) {
@@ -58,23 +61,17 @@ unsigned char cpu_ram_read(struct cpu *cpu, unsigned char index) {
 void cpu_ram_write(struct cpu *cpu, unsigned char index, unsigned char value) {
   cpu->ram[index] = value;
 }
-// void handle_LDI(struct cpu *cpu, unsigned int index, unsigned char value)
-// {
-//   cpu->reg[index] = value;
-// }
-// void handle_PRN(struct cpu *cpu, unsigned int index)
-// {
-//   printf("%d\n", cpu->reg[index]);
-// }
-void handle_PUSH(struct cpu *cpu, unsigned int index)
+
+void handle_PUSH(struct cpu *cpu, unsigned char value)
 {
   cpu->reg[7]--;
-  cpu_ram_write(cpu, cpu->reg[7], cpu->reg[index]);
+  cpu_ram_write(cpu, cpu->reg[7], value);
 }
-void handle_POP(struct cpu *cpu, unsigned int index)
+unsigned char handle_POP(struct cpu *cpu, unsigned char value)
 {
-  cpu->reg[index] = cpu_ram_read(cpu, cpu->reg[7]);
+  value = cpu_ram_read(cpu, cpu->reg[7]);
   cpu->reg[7]++;
+  return value;
 }
 
 /**
@@ -84,7 +81,6 @@ void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
-  
   while (running) {
     // TODO
     unsigned char operandA = 0;
@@ -102,15 +98,14 @@ void cpu_run(struct cpu *cpu)
     } else {
       return;
     }
-    
+    int cur_index = ir;
     printf("TRACE: cpu-PC: %d: cpu-IR: %02X    operand0: %02x operand1: %02x\n", cpu->pc, ir, operandA, operandB);
-   
-   int instracton_set_pc = (ir >> 4) & 1;
     // 4. switch() over it to decide on a course of action.
     switch (ir) {
     // 5. Do whatever the instruction should do according to the spec.
     case HLT:
       running = 0;
+      cpu->pc += 1;
       break;
     case LDI:
       cpu->reg[operandA] = operandB;
@@ -122,22 +117,57 @@ void cpu_run(struct cpu *cpu)
         alu(cpu, ALU_MUL, operandA, operandB);
         break;
     case PUSH:
-      handle_PUSH(cpu, operandA);
-      break;
+        handle_PUSH(cpu, cur_index);
+        break;
     case POP:
-      handle_POP(cpu, operandA);
-      break;
+        handle_POP(cpu, cur_index);
+        break;
+    case CALL:
+        cpu->reg[7]--;
+        cpu->ram[cpu->reg[7]] = cpu->pc + 2;
+        cpu->pc = cpu->reg[operandA];
+        num_operands = -1;
+        break;
+    case RET:
+        cpu->pc = cpu->ram[cpu->reg[7]];
+        cpu->reg[7]++;
+        break;
+    case ADD:
+        alu(cpu, ALU_ADD, operandA, operandB);
+    case CMP:
+	      if (cpu->reg[operandA] == cpu->reg[operandB]) {
+	        cpu->fl = 1;
+	      }
+	      else if (cpu->reg[operandA] < cpu->reg[operandB]) {
+	        cpu->fl = 4;
+	      }
+	      else if (cpu->reg[operandA] > cpu->reg[operandB]) {
+	        cpu->fl = 2;
+	      }
+	      break;
+    case JMP:
+        cpu->pc = cpu->reg[operandA] - num_operands - 1;
+        break;
+    case JEQ:
+	      if (cpu->fl == 1) {
+	      cpu->pc = cpu->reg[operandA] - num_operands - 1;
+	      }
+	      break;
+	  case JNE:
+	      if (cpu->fl == 2 || cpu->fl == 4) {
+	      cpu->pc = cpu->reg[operandA] - num_operands - 1;
+	      }
+	      break;
     default:
       printf("Error Unknown Instruction");
-      break;
+      exit(1);
     }
     
     // 6. Move the PC to the next instruction.
-    if (!instracton_set_pc) {
-     cpu->pc += (num_operands+1);
-    }
+     cpu->pc += (num_operands + 1);
   }
 }
+
 
 /**
  * Initialize a CPU struct
@@ -147,9 +177,9 @@ void cpu_init(struct cpu *cpu)
   // TODO: Initialize the PC and other special registers
   // cpu = malloc(sizeof(struct cpu));
   cpu->pc = 0;
-  cpu->f1 = 0;
+  cpu->fl = 0;
   memset(cpu->reg, 0, sizeof(cpu->reg));
   memset(cpu->ram, 0 , sizeof(cpu->ram));
- // cpu->reg[7] = 0xF4;
+  cpu->reg[7] = 0xF4;
 
 }
--- ls8/cpu.h
+++ ls8/cpu.h
@@ -11,13 +11,14 @@ struct cpu {
   // ram (array)
   unsigned char ram[256];
   // flag reg
-  unsigned char f1;
+  unsigned char fl;
 };
 
 // ALU operations
 enum alu_op {
-	ALU_MUL
+	ALU_MUL,
 	// Add more here
+  ALU_ADD
 };
 
 // Instructions
@@ -30,10 +31,17 @@ enum alu_op {
 // TODO: more instructions here. These can be used in cpu_run().
 #define HLT  0b00000001
 #define MUL  0b10100010
+#define ADD  0b10100000
 #define PUSH 0b01000101
 #define POP  0b01000110 
-// Function declarations
+#define CALL 0b01010000
+#define RET  0b00010001
 
+#define CMP 0b10100111
+#define JMP 0b01010100
+#define JNE 0b01010110
+#define JEQ 0b01010101
+// Function declarations
 extern void cpu_load(struct cpu *cpu, char *file);
 extern void cpu_init(struct cpu *cpu);
 extern void cpu_run(struct cpu *cpu);
